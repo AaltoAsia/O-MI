97c913e60cdcd6477f63c8b6ec5b9a9220a952dc
Tuomas Kinnunen <tuomas.kinnunen@paivola.fi>
2014-12-02 20:19:49 +0200

    spray test and foundation for our server

---
+++ O-MI Node/src/main/scala/http/OmiService.scala
@@ -0,0 +1,38 @@
+package http
+
+import akka.actor.Actor
+import spray.routing._
+import spray.http._
+import MediaTypes._
+
+class OmiServiceActor extends Actor with OmiService {
+
+  // the HttpService trait defines only one abstract member, which
+  // connects the services environment to the enclosing actor or test
+  def actorRefFactory = context
+
+  // this actor only runs our route, but you could add
+  // other things here, like request stream processing
+  // or timeout handling
+  def receive = runRoute(myRoute)
+}
+
+
+// this trait defines our service behavior independently from the service actor
+trait OmiService extends HttpService {
+
+  val myRoute =
+    path("") { // Root
+      get {
+        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            <html>
+              <body>
+                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
+              </body>
+            </html>
+          }
+        }
+      }
+    }
+}
