30a1a445562993d4e4e2be0bfdf3c397a284272a
Shan Yuanqi <shany1@niksula.hut.fi>
2014-12-02 20:41:05 +0200

    Initialize webform
    Homepage, css, javascript

---
+++ WebForm/JS/submit.js
@@ -0,0 +1,11 @@
+/* What to do with the form submissal */
+function submit(){
+	var id = $("#sensorId").val();
+	var location = $("#location").val();
+	console.log("ID: " + id + ", Location: " + location);
+	
+	//TBT
+	var xmlDocument = $("<xml/>");
+	
+	
+}
