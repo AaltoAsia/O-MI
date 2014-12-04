43425e70d85c790788f6262a0c19258b16e421c1
Jussi Pirilä <jupeti10@gmail.com>
2014-12-04 20:07:05 +0200

    Small modifications on SensorAggregator.scala.

--- O-MI Node/src/main/scala/SensorAggregator/SensorAggregator.scala
+++ O-MI Node/src/main/scala/SensorAggregator/SensorAggregator.scala
@@ -1,30 +1,33 @@
-
-/** Class to convert sensor path and data to OM-I write request
-  * 
-  * @tparam type of sensor data
-  * @param Path to were sensor is. Last part is used as ID
-  * @param Data value from the sensor
-  * @return Returns the O-MI write request XML
-  */
-class SensorData[T](val path:String,val data:T) {
-	val id = path.split("/").last
-def getWriteRequest():scala.xml.Elem ={
-	  var writeRequest =
-	    <omi:omiEnvelope ttl="0" version="1.0">
-			  <omi:write msgformat="odf">
-			  <omi:msg>
-	    <Objects>
-			  <Object>
-		  <id>{id}</id>
-	  <Infoitem name="Sensor Data">
-	  <value>{data}</value>
-	  </Infoitem>
-	  </Object>
-</Objects>
-</omi:msg>
-</omi:write>
-</omi:omiEnvelope>
-	 return writeRequest
-	}
+package SensorAggregator
+import SensorDataStructure.SensorData
+/**
+ * Class to convert sensor path and data to OM-I write request
+ *
+ * @tparam type of sensor data
+ * @param Path to were sensor is. Last part is used as ID
+ * @param Data value from the sensor
+ * @return Returns the O-MI write request XML
+ */
+object SensorAggregator{
+  def getWriteRequest(sensorData: SensorData[_]): scala.xml.Elem = {
+    var writeRequest =
+      <omi:omiEnvelope ttl="0" version="1.0">
+        <omi:write msgformat="odf">
+          <omi:msg>
+            <Objects>
+              <Object>
+                <id>{ sensorData.path.split("/").last }</id>
+                <Infoitem name="Sensor Data">
+                  <value>{ sensorData.value }</value>
+                </Infoitem>
+              </Object>
+            </Objects>
+          </omi:msg>
+        </omi:write>
+      </omi:omiEnvelope>
+    return writeRequest
+  }
 }
+ 
+
 
