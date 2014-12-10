629fb4388a7c1b981602c65c9405a32575b5efc9
OJ04 <ville.ojaniemi@elisanet.fi>
2014-12-10 14:37:12 +0200

    Added send prototype to DataAggregator

--- O-MI Node/src/main/scala/dataAggregator/DataAggregator.scala
+++ O-MI Node/src/main/scala/dataAggregator/DataAggregator.scala
@@ -1,9 +1,12 @@
 package dataAggregator
 
 import akka.actor.Actor
-
+import akka.actor.ActorRef
 class DataAggregator extends Actor {
 
   def receive = ???
+  def send(omidata:scala.xml.Elem,target:ActorRef) ={
+    target ! omidata
+  }
   
 }
