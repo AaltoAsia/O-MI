0d2829e492856e6721969301763abc40090a3233
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-01-09 17:42:31 +0200

    New simple generic agent, forwards standard input, need testing

--- O-MI Node/src/main/scala/agentSystemInterface/agents/GenericAgent.scala
+++ O-MI Node/src/main/scala/agentSystemInterface/agents/GenericAgent.scala
@@ -1,16 +1,17 @@
 package agents
-import akka.actor.{ Actor, ActorRef, Props }
+import akka.actor.{ ActorSystem, Actor, ActorRef, Props }
 import akka.io.{ IO, Tcp }
 import akka.util.ByteString
 import java.net.InetSocketAddress
 import xml._
+import io._
 
 object GenericAgentClient {
-  def props(remote: InetSocketAddress, replies: ActorRef) =
-  Props(classOf[GenericAgentClient], remote, replies)
+  def props(remote: InetSocketAddress) =
+  Props(classOf[GenericAgentClient], remote)
 }
 
-class GenericAgentClient(remote: InetSocketAddress, listener: ActorRef) extends Actor{
+class GenericAgentClient(remote: InetSocketAddress) extends Actor{
  import Tcp._
  import context.system
   
@@ -18,27 +19,52 @@ class GenericAgentClient(remote: InetSocketAddress, listener: ActorRef) extends 
  
   def receive = {
     case CommandFailed(_: Connect) =>
-      listener ! "connect failed"
+      println("connect failed")
       context stop self
        
     case c @ Connected(remote, local) =>
-      listener ! c
+      println(c.toString)
       val connection = sender()
       connection ! Register(self)
       context become {
-        case data: xml.Node => 
+        case data: Node=> 
           connection ! Write( ByteString( new PrettyPrinter( 80, 2 ).format( data ) ) )
         case CommandFailed(w: Write) => 
         // O/S buffer was full
-          listener ! "write failed"
+          println("write failed")
         case Received(data) =>
-          listener ! data
+          println(data.toString)
         case "close" =>
           connection ! Close
         case _: ConnectionClosed =>
-          listener ! "connection closed"
+          println("connection closed")
           context stop self
       }
   }
 }
 
+class GenericAgent(args: Array[String]) extends App {
+  if(args.length < 3){
+    println("arguments are <address> <port> <sensor's path>")
+  } else {
+    val address = new InetSocketAddress( args(0),args(1).toInt)
+    var path = args(2).split("/")
+    
+    if(path.head == "Objects")
+      path = path.tail
+
+    implicit val system = ActorSystem("on-generic-agent")
+    val client = system.actorOf(GenericAgentClient.props(address), "generic-agent")  
+
+    while(true){
+      val value = StdIn.readLine
+			client ! <Objects>{genODF(path,value)}</Objects>
+    }
+  }
+  def genODF( path: Seq[String], value: String) : Elem ={
+    if(path.length == 1)
+      <InfoItem name={path.head}><value>{value}</value></InfoItem>
+    else
+      <Object><id>{path.head}</id>{genODF(path.tail,value)}</Object>
+  }
+}
