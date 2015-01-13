a1001180c7d78d310987cdab9dc3034ec0555aeb
OJ04 <ville.ojaniemi@elisanet.fi>
2015-01-13 16:58:48 +0200

    fixed more issues

--- O-MI Node/src/test/scala/database/SQLiteTest.scala
+++ O-MI Node/src/test/scala/database/SQLiteTest.scala
@@ -8,16 +8,17 @@ object SQLiteTest extends Specification {
   "SQLite" should {
     sequential
     var data1 = new database.DBSensor("path/to/sensor/temp1","25.5C",new java.sql.Timestamp(new java.util.Date().getTime))
-
-    database.SQLite.set(data1)
-    
     "return correct value for given valid path" in {
+      database.SQLite.set(data1)
        database.SQLite.get("path/to/sensor/temp1").get.value shouldEqual "25.5C"
     }
     
-    database.SQLite.remove("path/to/sensor/temp1")
+    "return correct value for invalid path" in {
+       database.SQLite.get("invalid/path/to/sensor/temp1") shouldEqual None
+    }
     
-    "return correct value for given invalid path" in {
+    "return correct value for deleted path" in {
+      database.SQLite.remove("path/to/sensor/temp1")
        database.SQLite.get("path/to/sensor/temp1") shouldEqual None
     }
   }
