8dc8e0b8101dfb2fc1129ca36676bd1e43c94ad2
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-01-13 19:09:58 +0200

    Changed sturcutere. Added new thread. Closes on connection failure and close.

--- Agents/GenericAgent.scala
+++ Agents/GenericAgent.scala
@@ -1,10 +1,13 @@
 package agents
-import akka.actor.{ ActorSystem, Actor, ActorRef, Props }
+import akka.actor.{ ActorSystem, Actor, ActorRef, Props, Terminated }
 import akka.io.{ IO, Tcp }
 import akka.util.ByteString
 import java.net.InetSocketAddress
 import xml._
 import io._
+import scala.concurrent.duration._
+import akka.util.Timeout
+import akka.pattern.ask
 
 object GenericAgentClient {
   def props(remote: InetSocketAddress) =
@@ -21,6 +24,7 @@ class GenericAgentClient(remote: InetSocketAddress) extends Actor{
     case CommandFailed(_: Connect) =>
       println("connect failed")
       context stop self
+      system.shutdown()
        
     case c @ Connected(remote, local) =>
       println(c.toString)
@@ -39,12 +43,35 @@ class GenericAgentClient(remote: InetSocketAddress) extends Actor{
         case _: ConnectionClosed =>
           println("connection closed")
           context stop self
+          system.shutdown()
       }
   }
 }
+class GenericAgent(path: Seq[String], client: ActorRef) extends Actor {
 
-object GenericAgent extends App {
-  override def main(args: Array[String]) = {
+  self ! "Run"
+  def receive = {
+    case "Run" => run()
+  }
+  def run() = {
+    if(System.in.available() != 0){
+      val value = StdIn.readLine
+      client ! <Objects>{genODF(path,value)}</Objects>
+    }
+    self ! "Run"
+  }
+
+
+  def genODF( path: Seq[String], value: String) : Elem ={
+    if(path.length == 1)
+      <InfoItem name={path.head}><value>{value}</value></InfoItem>
+    else
+       <Object><id>{path.head}</id>{genODF(path.tail,value)}</Object>
+  }
+}
+object GenericMain {
+  def main(args: Array[String]) = {
+    import scala.concurrent.ExecutionContext.Implicits.global
     if(args.length < 3){
       println("arguments are <address> <port> <sensor's path>")
     } else {
@@ -53,20 +80,10 @@ object GenericAgent extends App {
       
       if(path.head == "Objects")
         path = path.tail
-
+      implicit val timeout = Timeout(5.seconds)
       implicit val system = ActorSystem("on-generic-agent")
-      val client = system.actorOf(GenericAgentClient.props(address), "generic-agent")  
-
-      while(true){
-        val value = StdIn.readLine
-        client ! <Objects>{genODF(path,value)}</Objects>
-      }
-    }
-    def genODF( path: Seq[String], value: String) : Elem ={
-      if(path.length == 1)
-        <InfoItem name={path.head}><value>{value}</value></InfoItem>
-      else
-        <Object><id>{path.head}</id>{genODF(path.tail,value)}</Object>
+      val client = system.actorOf(GenericAgentClient.props(address), "generic-agent-client")  
+      val agent = system.actorOf(Props[GenericAgent](new GenericAgent(path,client)), "generic-agent")
     }
   }
 }
