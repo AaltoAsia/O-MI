1e1d673b49e18d95bf96c9c7aab2ca2c913249ed
OJ04 <ville.ojaniemi@elisanet.fi>
2015-01-14 12:06:07 +0200

    sqlite get and set fully functional

--- O-MI Node/src/test/scala/database/SQLiteTest.scala
+++ O-MI Node/src/test/scala/database/SQLiteTest.scala
@@ -7,12 +7,48 @@ object SQLiteTest extends Specification {
   
   "SQLite" should {
     sequential
-    var data1 = new database.DBSensor("path/to/sensor/temp1","25.5C",new java.sql.Timestamp(new java.util.Date().getTime))
-    "return correct value for given valid path" in {
-      database.SQLite.set(data1)
-       database.SQLite.get("path/to/sensor/temp1").get.value shouldEqual "25.5C"
-    }
     
+    database.SQLite.set(database.DBSensor("path/to/sensor1/temp1","25.5C",new java.sql.Timestamp(new java.util.Date().getTime)))
+    database.SQLite.set(database.DBSensor("path/to/sensor1/temp2","25.5C",new java.sql.Timestamp(new java.util.Date().getTime)))
+    database.SQLite.set(database.DBSensor("path/to/sensor2/temp1","25.5C",new java.sql.Timestamp(new java.util.Date().getTime)))
+    database.SQLite.set(database.DBSensor("path/to/sensor2/temp2","25.5C",new java.sql.Timestamp(new java.util.Date().getTime)))
+    
+    "return correct value for given valid path" in {
+        var res = ""
+        
+       database.SQLite.get("path/to/sensor1/temp1") match
+       {
+        case Some(obj) =>
+          obj match{
+            case DBSensor(p:String,v:String,t:java.sql.Timestamp) =>
+              res = v
+          }
+        case None =>
+          res = "not found"
+       }
+       res shouldEqual "25.5C"
+    }
+    "return correct objects for given valid path" in {
+        var res = Array[String]()
+        
+       database.SQLite.get("path/to/sensor1") match
+       {
+        case Some(obj) =>
+          obj match{
+            case ob:DBObject =>
+              var i = 0
+              res = Array.ofDim[String](ob.childs.length)
+              for(o <- ob.childs)
+              {
+                res(i) = o.path
+                i += 1
+              }
+          }
+        case None =>
+          
+       }
+       res shouldEqual Array("path/to/sensor1/temp1","path/to/sensor1/temp2")
+    }
     "return correct value for invalid path" in {
        database.SQLite.get("invalid/path/to/sensor/temp1") shouldEqual None
     }
@@ -21,5 +57,12 @@ object SQLiteTest extends Specification {
       database.SQLite.remove("path/to/sensor/temp1")
        database.SQLite.get("path/to/sensor/temp1") shouldEqual None
     }
+    "find added object" in{
+      database.SQLite.hasObject("path") shouldEqual true
+    }
+    "find added object" in{
+      database.SQLite.hasObject("path/to") shouldEqual true
+    }
+    
   }
 }
