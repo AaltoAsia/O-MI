130ea1b0826a2767698f0c163fcece3eeac7097c
OJ04 <ville.ojaniemi@elisanet.fi>
2015-01-20 16:44:43 +0200

    commenting and scaladoc

--- O-MI Node/src/test/scala/database/SQLiteTest.scala
+++ O-MI Node/src/test/scala/database/SQLiteTest.scala
@@ -8,15 +8,31 @@ object SQLiteTest extends Specification {
   "SQLite" should {
     sequential
     
-    database.SQLite.set(database.DBSensor("path/to/sensor1/temp1","25.5C",new java.sql.Timestamp(new java.util.Date().getTime)))
-    database.SQLite.set(database.DBSensor("path/to/sensor1/temp2","25.5C",new java.sql.Timestamp(new java.util.Date().getTime)))
-    database.SQLite.set(database.DBSensor("path/to/sensor2/temp1","25.5C",new java.sql.Timestamp(new java.util.Date().getTime)))
-    database.SQLite.set(database.DBSensor("path/to/sensor2/temp2","25.5C",new java.sql.Timestamp(new java.util.Date().getTime)))
-    
-    "return correct value for given valid path" in {
+    var data1 = DBSensor("path/to/sensor1/temp","21.5C",new java.sql.Timestamp(new java.util.Date().getTime))
+    var data2 = DBSensor("path/to/sensor1/hum","40%",new java.sql.Timestamp(new java.util.Date().getTime))
+    var data3 = DBSensor("path/to/sensor2/temp","24.5",new java.sql.Timestamp(new java.util.Date().getTime))
+    var data4= DBSensor("path/to/sensor2/hum","60%",new java.sql.Timestamp(new java.util.Date().getTime))
+    var data5 = DBSensor("path/to/sensor1/temp","21.6C",new java.sql.Timestamp(new java.util.Date().getTime))
+   
+    "return true when adding new data" in {
+      database.SQLite.set(data1) shouldEqual true
+    }
+    "return true when adding new data" in {
+      database.SQLite.set(data2) shouldEqual true
+    }
+    "return true when adding new data" in {
+      database.SQLite.set(data3) shouldEqual true
+    }
+    "return true when adding new data" in {
+      database.SQLite.set(data4) shouldEqual true
+    }
+    "return false when updating old data" in {
+      database.SQLite.set(data5) shouldEqual false
+    }
+     "return correct value for given valid path" in {
         var res = ""
         
-       database.SQLite.get("path/to/sensor1/temp1") match
+       database.SQLite.get("path/to/sensor1/hum") match
        {
         case Some(obj) =>
           obj match{
@@ -26,11 +42,26 @@ object SQLiteTest extends Specification {
         case None =>
           res = "not found"
        }
-       res shouldEqual "25.5C"
+       res shouldEqual "40%"
     }
-    "return correct objects for given valid path" in {
-        var res = Array[String]()
+    "return correct value for given valid updated path" in {
+        var res = ""
         
+       database.SQLite.get("path/to/sensor1/temp") match
+       {
+        case Some(obj) =>
+          obj match{
+            case DBSensor(p:String,v:String,t:java.sql.Timestamp) =>
+              res = v
+          }
+        case None =>
+          res = "not found"
+       }
+       res shouldEqual "21.6C"
+    }
+    
+    "return correct childs for given valid path" in {
+        var res = Array[String]()
        database.SQLite.get("path/to/sensor1") match
        {
         case Some(obj) =>
@@ -45,28 +76,35 @@ object SQLiteTest extends Specification {
               }
           }
         case None =>
-          
        }
-       res shouldEqual Array("path/to/sensor1/temp1","path/to/sensor1/temp2")
+       res.length == 2 && res.contains("path/to/sensor1/temp") && res.contains("path/to/sensor1/hum") shouldEqual true
     }
-    "return correct value for invalid path" in {
-       database.SQLite.get("invalid/path/to/sensor/temp1") shouldEqual None
+    "return None for given invalid path" in {
+      database.SQLite.get("path/to/nosuchsensor") shouldEqual None
     }
     
-    "return correct value for deleted path" in {
-      database.SQLite.remove("path/to/sensor1/temp1")
-       database.SQLite.get("path/to/sensor1/temp1") shouldEqual None
+    "return true when removing valid path" in{
+      database.SQLite.remove("path/to/sensor1/hum") shouldEqual true
     }
-    "return correct value for deleted path" in {
-      database.SQLite.remove("path/to/sensor1/temp2")
-       database.SQLite.get("path/to/sensor1/temp2") shouldEqual None
+    "return true when removing valid path" in{
+      database.SQLite.remove("path/to/sensor1/temp") shouldEqual true
     }
-    "return correct value for deleted path" in {
-       database.SQLite.get("path/to/sensor1") shouldEqual None
+    "return false when trying to remove object from the middle" in{
+      database.SQLite.remove("path/to/sensor2") shouldEqual false
     }
-    "return correct value for deleted path" in {
-       database.SQLite.get("path/to") shouldNotEqual None
+    "return true when removing valid path" in{
+      database.SQLite.remove("path/to/sensor2/temp") shouldEqual true
     }
+    "return true when removing valid path" in{
+      database.SQLite.remove("path/to/sensor2/hum") shouldEqual true
+    }
+     "return None when searching non existent object" in{
+      database.SQLite.get("path/to/sensor2") shouldEqual None
+    }
+     "return None when searching non existent object" in{
+      database.SQLite.get("path/to/sensor1") shouldEqual None
+    }
+   
     
   }
 }
