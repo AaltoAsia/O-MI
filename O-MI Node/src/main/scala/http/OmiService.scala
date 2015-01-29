d8f66f8be04bb1150851c3217d2900749958785c
Kinnunen Tuomas <tuomas.kinnunen@aalto.fi>
2015-01-29 16:11:05 +0200

    Added some info and links to helloworld page

--- O-MI Node/src/main/scala/http/OmiService.scala
+++ O-MI Node/src/main/scala/http/OmiService.scala
@@ -47,6 +47,11 @@ trait OmiService extends HttpService with CORSDirectives
               <html>
                 <body>
                   <h1>Say hello to <i>O-MI Node service</i>!</h1>
+                  <a href="/Objects">Url Data Discovery /Objects: Root of the hierarchy</a>
+                  <p>With url data discovery you can discover or request Objects,
+                     InfoItems and values with HTTP Get request by giving some existing
+                     path to the O-DF xml hierarchy.</p>
+                  <a href="/html/form.html">O-MI Test Client WebApp</a>
                 </body>
               </html>
             }
