f3c6e1f96b03c52405c3f27d502282cc510684b0
Kinnunen Tuomas <tuomas.kinnunen@aalto.fi>
2015-02-03 17:36:35 +0200

    add a Path class to reduce errors with paths

--- O-MI Node/src/main/scala/parsing/Types.scala
+++ O-MI Node/src/main/scala/parsing/Types.scala
@@ -28,3 +28,48 @@ case object MetaData extends ODFNodeType
  */
 case class ODFNode( path: String, nodeType: ODFNodeType, value: Option[String], time: Option[String], metadata: Option[String])
 
+
+/**
+ * Path is a wrapper for Seq[String] representing path to an O-DF Object
+ * It abstracts path seperators ('/') from error prone actions such as joining
+ * two paths or creating new Paths from user input.
+ * Path can be used as a sequence via an implicit conversion or _.toSeq
+ */
+class Path(pathSeq: Seq[String]){
+  import Path._
+  val toSeq = {
+    val normalized = pathSeq.filterNot(_ == "")
+    normalized.toSeq
+  }
+
+  def this(pathStr: String) = this{
+    pathStr.split("/")
+  }
+
+  /**
+   * Join two paths.
+   * @param otherPath other path to join at the end of this one
+   * @return new path with other joined to this path
+   */
+  def /(otherPath: Path): Path = Path(this ++ otherPath)
+
+  def /(otherPathStr: String): Path = {
+    this / Path(otherPathStr)
+  }
+
+  /**
+   * Creates a path string which represents this path with '/' seperators.
+   * Representation doesn't start nor end with a '/'.
+   */
+  override def toString: String = this.mkString("/")
+}
+
+
+object Path {
+  def apply(pathStr: String): Path = new Path(pathStr)
+  def apply(pathSeq: Seq[String]): Path = new Path(pathSeq)
+
+  import scala.language.implicitConversions // XXX: maybe a little bit stupid place for this
+
+  implicit def PathAsSeq(p: Path): Seq[String] = p.toSeq
+}
