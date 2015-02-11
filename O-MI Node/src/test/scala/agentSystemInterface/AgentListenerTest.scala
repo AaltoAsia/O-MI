f72322ea84511ab88ef36530480f73f3e22ec47c
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-02-11 15:08:19 +0200

    Removed wrong file, path changed for better place

--- O-MI Node/src/test/scala/agentSystemInterface/AgentListenerTest.scala
+++ O-MI Node/src/test/scala/agentSystemInterface/AgentListenerTest.scala
@@ -28,7 +28,7 @@ def after = SQLite.clearDB()
 
   val local = new InetSocketAddress("localhost", 1234)
   val remote = new InetSocketAddress("remote", 4321)
-  lazy val testOdf = Source.fromFile("src/test/scala/testOdf.xml").getLines().mkString("")
+  lazy val testOdf = Source.fromFile("src/test/scala/agentSystemInterface/testOdf.xml").getLines().mkString("")
 
   "AgentListener" should {
 
