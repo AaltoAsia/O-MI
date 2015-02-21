47dd291da5a6574fdfa402f022e5c0b3fc34affe
Kinnunen Tuomas <tuomas.kinnunen@aalto.fi>
2015-02-22 01:40:48 +0200

    cleared compile errors and cleared some dirty code

--- O-MI Node/src/main/scala/responses/SubscriptionHandlerActor.scala
+++ O-MI Node/src/main/scala/responses/SubscriptionHandlerActor.scala
@@ -7,13 +7,10 @@ import responses._
 
 import scala.collection.mutable.PriorityQueue
 import database._
+import parsing.Types.Subscription
 
 import java.sql.Timestamp
-import akka.io.{ IO, Tcp }
-import akka.util.ByteString
-import java.net.InetSocketAddress
 import xml._
-import io._
 import scala.math.Ordering
 
 import scala.concurrent.duration._
@@ -40,7 +37,6 @@ class SubscriptionHandlerActor extends Actor {
     */
 import scala.concurrent.ExecutionContext.Implicits.global
   implicit val timeout = Timeout(5.seconds)
-  import Tcp._
   import context.system
   type TimedSub = (Timestamp,Int) 
   object SubscriptionOrdering extends Ordering[TimedSub] {
@@ -52,36 +48,26 @@ import scala.concurrent.ExecutionContext.Implicits.global
   override def receive = { 
     case subscription: Subscription => {
       val (requestid, xmlanswer) = OMISubscription.setSubscription(subscription)
-      subscriptions ++= (new java.sql.Timestamp(System.currentTimeMillis()), requestid)
+      subscriptions += ((new java.sql.Timestamp(System.currentTimeMillis()), requestid))
       //send xml to the address this came from?
     }
   }
   
   def looper = {
-    var ids = subscriptions.takeWhile(sub => sub._1.getTime <= System.currentTimeMillis())
-    for( id <- ids){
-      //val dbSensors = SQLite.getSubData(id._2)
+    var activeSubs = subscriptions.takeWhile(_._1.getTime <= System.currentTimeMillis())
+
+    for( (time, id) <- activeSubs){
+      //val dbSensors = SQLite.getSubData(id)
       //right now subscription omi generation uses the paths in the dbsub, not sure if getSubData-function is needed as this looper
       //loops through all subs in the interval already? Also with the paths its easier to construct the OMI hierarchy
-      val sub = SQLite.getSub(id._2) 
+      val sub = SQLite.getSub(id).get 
       val omiMsg = generateOmi(id)
-      val callback = sub.get.callback.get.split(":").dropRight(1).mkString(":")
-      val port = sub.get.callback.get.split(":").takeRight(1).head.toInt
-      //callback addres were to send data
-      val address : InetSocketAddress = new InetSocketAddress(callback, port)
-      /**TODO: 
-        * Need away to get ActorRef of connection for sending data to address
-        * Untested possible hack for it bellow
-      */
-      (IO(Tcp) ? Connect(address) ).onSuccess {
-        case connected : Connected =>
-        val connection = sender()
-        connection ! Write( ByteString(
-            new PrettyPrinter( 80, 2 ).format( omiMsg )
-          )
-        )
-      }
-      id._1.setTime(id._1.getTime + sub.get.interval)
+      val callbackAddr = sub.callback.get
+
+      // send
+      CallbackHandlers.sendCallback(callbackAddr, omiMsg)
+
+      time.setTime(time.getTime + sub.interval)
     }
   }
 
