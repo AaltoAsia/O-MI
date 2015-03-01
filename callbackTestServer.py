25e5255fc0516d479360eafc7ee9466d530f60c8
Kinnunen Tuomas <tuomas.kinnunen@aalto.fi>
2015-03-01 17:11:55 +0200

    added colors to callback tester

--- callbackTestServer.py
+++ callbackTestServer.py
@@ -1,14 +1,45 @@
 #!/usr/bin/python3
 
 from http.server import *
+#from xml.dom.minidom import parseString
+
+# Use colors if available
+try:
+    from colorama import init, Fore, Style
+    init()
+except ImportError:
+    class Noop:
+        def __getattr__(s, key):
+            return ""
+    class Fore(Noop): pass
+    class Style(Noop): pass
+
+responseNum = 1
+
+def seperator():
+    return Style.BRIGHT +\
+    Fore.BLUE + "=" * 20 +\
+    Fore.YELLOW + " # {:02} # ".format(responseNum) +\
+    Fore.BLUE + "=" * 20 + Style.RESET_ALL
+
 
 class CallbackHandler(BaseHTTPRequestHandler):
     def do_POST(s):
         s.send_response(200)
         s.end_headers()
+
         content_len = int(s.headers['content-length'])
         post_body = s.rfile.read(content_len)
-        print(post_body.decode("UTF-8"))
+        msg = post_body.decode("UTF-8")
+
+        print(seperator())
+        #print(parseString(msg).toprettyxml("  "))
+        print(msg)
+        print(seperator() + "\n")
+        print(Fore.GREEN + Style.BRIGHT)
+
+        global responseNum
+        responseNum += 1
 
 server_address = ('', 5432)
 httpd = HTTPServer(server_address, CallbackHandler)
