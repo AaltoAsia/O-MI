1aa04d6b5a3318eebdd9caaa843e5833ecd90803
OJ04 <ville.ojaniemi@elisanet.fi>
2015-03-03 20:26:58 +0200

    fix for some database tests being performance dependant

--- O-MI Node/src/test/scala/database/DataFormaterTest.scala
+++ O-MI Node/src/test/scala/database/DataFormaterTest.scala
@@ -24,7 +24,7 @@ object DataFormaterTest extends Specification {
   SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-264C",new Timestamp(timeNow-1000)))
   SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-263C",new Timestamp(timeNow)))
   "return rigth values when enough values are present" in {
-     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2)
+     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2,Some(new Timestamp(timeNow)))
      fdata.length shouldEqual 5
      fdata(0).value shouldEqual "-271C"
      fdata(4).value shouldEqual "-263C"
@@ -38,7 +38,7 @@ object DataFormaterTest extends Specification {
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-270C",new Timestamp(timeNow-7000)))
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-269C",new Timestamp(timeNow-6000)))
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-268C",new Timestamp(timeNow-5000)))
-     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2)
+     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2,Some(new Timestamp(timeNow)))
      fdata.length shouldEqual 5
      fdata(0).value shouldEqual "-271C"
      fdata(4).value shouldEqual "-268C"
@@ -52,7 +52,7 @@ object DataFormaterTest extends Specification {
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-270C",new Timestamp(timeNow-3000)))
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-269C",new Timestamp(timeNow-2000)))
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-268C",new Timestamp(timeNow-1000)))
-     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2)
+     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2,Some(new Timestamp(timeNow)))
      fdata.length shouldEqual 5
      fdata(0).value shouldEqual "-272C"
      fdata(4).value shouldEqual "-268C"
@@ -65,7 +65,7 @@ object DataFormaterTest extends Specification {
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-270C",new Timestamp(timeNow-2000)))
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-269C",new Timestamp(timeNow-1000)))
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-268C",new Timestamp(timeNow)))
-     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2)
+     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2,Some(new Timestamp(timeNow)))
      fdata.length shouldEqual 5
      fdata(0) shouldEqual null
      fdata(4).value shouldEqual "-268C"
@@ -76,7 +76,7 @@ object DataFormaterTest extends Specification {
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-272C",new Timestamp(timeNow-13000)))
      SQLite.set(new DBSensor(Path("path/to/test/sensor1"),"-271C",new Timestamp(timeNow-12000)))
   
-     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2)
+     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2,Some(new Timestamp(timeNow)))
      fdata.length shouldEqual 5
      fdata(0).value shouldEqual "-271C"
      fdata(4).value shouldEqual "-271C"
@@ -84,7 +84,7 @@ object DataFormaterTest extends Specification {
     "return empty array if absolutely no data is found" in {
      SQLite.remove(Path("path/to/test/sensor1"))
      timeNow = new java.util.Date().getTime
-     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2)
+     var fdata = DataFormater.FormatSubData(Path("path/to/test/sensor1"),new Timestamp(timeNow-10000), 2,Some(new Timestamp(timeNow)))
      fdata.length shouldEqual 0
   }
   }
