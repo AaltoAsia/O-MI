0a7f3b55440ec1f903bdf98cff52439d855bca97
Jussi Pirilä <jussi.pirila@aalto.fi>
2015-03-04 02:28:16 +0200

    test for SubscriptionHandlerAgent

--- O-MI Node/src/test/scala/responses/SubscriptionHandlerActorTest.scala
+++ O-MI Node/src/test/scala/responses/SubscriptionHandlerActorTest.scala
@@ -1,12 +1,16 @@
 package responses
 
 import org.specs2.mutable._
-import akka.testkit.{TestKit, TestActorRef}
+import akka.testkit.{TestKit, TestActorRef, TestProbe}
 import akka.actor._
 import com.typesafe.config.ConfigFactory
 import org.specs2.specification.Scope
 import database.DBSub
 import parsing.Types.Path
+import scala.concurrent._
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration.Duration
 
 class SubscriptionHandlerActorTest extends Specification {
   
@@ -14,15 +18,45 @@ class SubscriptionHandlerActorTest extends Specification {
   akka.loggers = ["akka.testkit.TestEventListener"]
   """))) with Scope
 
-  val testSub1 = new DBSub(Array(Path("SubscriptionTest/test")), 2, 2,None,None)
+  val testSub1 = new DBSub(Array(Path("SubscriptionTest/test")), 60, 2,Some("test"),None)
   
-  val testID = database.SQLite.saveSub(testSub1)
-  
-  "SubscriptionHandlerActor" should{
-    "load given sub into memory when sent load message" in new Actors{
+  val testId = Promise[Int]
+
+  "SubscriptionHandlerActor" should {
+    sequential
+    "load given interval sub into memory when sent load message" in new Actors{
+      
       val subscriptionHandler = TestActorRef[SubscriptionHandlerActor]
       val subscriptionActor = subscriptionHandler.underlyingActor
+      val probe = TestProbe()
+//      testId.future
+      
+      testId.success(database.SQLite.saveSub(testSub1))
+
+      val futureId: Int = Await.result(testId.future, scala.concurrent.duration.Duration(1000,"ms"))
       subscriptionActor.eventSubs.isEmpty === true
+      subscriptionActor.intervalSubs.isEmpty === true
+      subscriptionHandler.tell(NewSubscription(futureId), probe.ref)
+
+      subscriptionActor.intervalSubs.isEmpty === false
+    } 
+    
+    "remove given sub from queue when sent remove message" in new Actors{
+      //this test probably fails if previous test fails
+      val subscriptionHandler = TestActorRef[SubscriptionHandlerActor]
+      val subscriptionActor = subscriptionHandler.underlyingActor
+      val probe = TestProbe()
+      
+      val futureId: Int = Await.result(testId.future, scala.concurrent.duration.Duration(1000,"ms"))
+
+      subscriptionActor.eventSubs.isEmpty === true
+      subscriptionActor.intervalSubs.isEmpty === false
+//      val answer = subscriptionHandler.ask(
+//          probe.ref, RemoveSubscription(futureId))(Timeout(Duration.apply(1500, "ms")))
+//      Await.result(answer, Duration.apply(1500, "ms")) === true
+      
+      subscriptionHandler.tell(RemoveSubscription(futureId), probe.ref)
+      probe.expectMsgType[Boolean](Duration.apply(2400,"ms")) === true
       subscriptionActor.intervalSubs.isEmpty === true
     }
   }
