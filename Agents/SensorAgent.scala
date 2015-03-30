5c39e2d224b2727b963e0f01f4ba902c60b4e802
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-03-30 12:33:33 +0300

    Fixed not adding sensor data to DB, switched from setMany to set.

--- Agents/SensorAgent.scala
+++ Agents/SensorAgent.scala
@@ -110,7 +110,7 @@ class SensorAgent(uri : String) extends AgentActor {
       system.log.debug("Data gained. Saving to Database.")
       if (!list.isEmpty) {
         // InfoItems filtered out
-        SQLite.setMany(list.filter(_._1.split('_').length > 3).map(item => {
+        val data = list.filter(_._1.split('_').length > 3).map(item => {
           val sensor: String = item._1
           val value: String = item._2 // Currently as string, convert to double?
           // Split name from underlines
@@ -120,8 +120,10 @@ class SensorAgent(uri : String) extends AgentActor {
           val path = if(split(0) == "vtt") split.dropRight(2) ++  split.takeRight(2).reverse
           else split
           println("Saving to path: " +"Objects/" + path.mkString("/"))
-          ("Objects/" + path.mkString("/"), TimedValue(Some(new Timestamp(date.getTime)),value))
-        }))
+          OdfInfoItem(Seq("Objects") ++ path.toSeq, Seq(TimedValue(Some(new Timestamp(date.getTime)),value)))
+        })
+        InputPusher.handleInfoItems(data);
+        //InputPusher.handlePathValuePairs(data);
       }
       system.log.debug("Successfully saved to Database.")
     }
