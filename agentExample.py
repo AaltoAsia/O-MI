ce45ebdd0ffc4955fc46a30da13a7a52b25ea20e
Kinnunen Tuomas <tuomas.kinnunen@aalto.fi>
2015-04-15 04:25:32 +0300

    made a new external agent with python

---
+++ agentExample.py
@@ -0,0 +1,20 @@
+#!/bin/env python3
+
+from socket import create_connection
+
+connection = create_connection(("localhost", 8181))
+
+while True:
+    data = input()
+    odf_message = b'''
+        <Objects>
+            <Object>
+                <id>ExampleAgent</id>
+                <InfoItem name="ExampleAgentSensor">
+                    <value>''' +\
+                    bytes(data, "utf-8") +\
+                b'''</value>
+                </InfoItem>
+            </Object>
+        </Objects>'''
+    connection.sendall(odf_message)
