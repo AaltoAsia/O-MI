c24f7693607506677b79b0255d2b56aad0b7a9d4
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-05-05 11:15:25 +0300

    InputPusherForDB is now Actor

--- O-MI Node/src/main/scala/agentSystem/InputPusher.scala
+++ O-MI Node/src/main/scala/agentSystem/InputPusher.scala
@@ -4,6 +4,7 @@ import database._
 import parsing.Types._
 import parsing.Types.Path._
 import java.util.Date
+import akka.actor._
 
 /** Trait for pushing data from sensors to db.
   *
@@ -20,7 +21,7 @@ trait InputPusher {
   /** Function for handling sequences of path and value pairs.
     *
     */
-  def handlePathValuePairs( pairs: Seq[(String,TimedValue)] ): Unit
+  def handlePathValuePairs( pairs: Seq[(Path,TimedValue)] ): Unit
   /** Function for handling sequences of path and MetaData pairs.
     *
     */
@@ -29,25 +30,46 @@ trait InputPusher {
 
 // XXX: FIXME: temp hack
 object InputPusher {
-  def handleObjects = 
-    new InputPusherForDB(new SQLiteConnection) handleObjects _
-
-  def handleInfoItems =
-    new InputPusherForDB(new SQLiteConnection) handleInfoItems _
-
-  def handlePathValuePairs =
-    new InputPusherForDB(new SQLiteConnection) handlePathValuePairs _
-
-  def handlePathMetaDataPairs =
-    new InputPusherForDB(new SQLiteConnection) handlePathMetaDataPairs _
+  def props() : Props = Props(new InputPusherForDB(new SQLiteConnection))
+  var ipdb : Option[ActorRef] = None //system.actorOf(props, "input-pusher-for-db")
+  def handleObjects(objs: Seq[OdfObject]) = { 
+    //new InputPusherForDB(new SQLiteConnection) handleObjects _
+    if(ipdb.nonEmpty)
+      ipdb.get ! HandleObjects(objs) 
+  }
+  def handleInfoItems(items: Seq[OdfInfoItem]) = {
+    //new InputPusherForDB(new SQLiteConnection) handleInfoItems _
+    if(ipdb.nonEmpty)
+      ipdb.get ! HandleInfoItems(items) 
+  }
+  def handlePathValuePairs(pairs: Seq[(Path,TimedValue)]) = {
+    //new InputPusherForDB(new SQLiteConnection) handlePathValuePairs _
+    if(ipdb.nonEmpty)
+      ipdb.get ! HandlePathValuePairs(pairs)
+  }
+  def handlePathMetaDataPairs(pairs: Seq[(Path,String)]) = {
+    //new InputPusherForDB(new SQLiteConnection) handlePathMetaDataPairs _
+    if(ipdb.nonEmpty)
+      ipdb.get ! HandlePathMetaDataPairs(pairs)
+  }
 }
 
+  case class HandleObjects(objs: Seq[OdfObject])
+  case class HandleInfoItems(items: Seq[OdfInfoItem])
+  case class HandlePathValuePairs(pairs: Seq[(Path,TimedValue)])
+  case class HandlePathMetaDataPairs(pairs: Seq[(Path,String)])
 /** Creates an object for pushing data from internal agents to db.
   *
   */
-class InputPusherForDB(val dbobject: DB) extends InputPusher{
+class InputPusherForDB(val dbobject: DB) extends Actor with ActorLogging with InputPusher{
+ 
 
-  
+  override def receive = {
+    case HandleObjects(objs) => handleObjects(objs)
+    case HandleInfoItems(items) => handleInfoItems(items)
+    case HandlePathValuePairs(pairs) => handlePathValuePairs(pairs)
+    case HandlePathMetaDataPairs(pairs) => handlePathMetaDataPairs(pairs)
+  }
   /** Function for handling sequences of OdfObject.
     *
     */
@@ -58,6 +80,7 @@ class InputPusherForDB(val dbobject: DB) extends InputPusher{
       if(obj.sensors.nonEmpty)
         handleInfoItems(obj.sensors)
     }
+    log.debug("Successfully saved Objects to DB")
   }
 
   /** Function for handling sequences of OdfInfoItem.
@@ -76,16 +99,19 @@ class InputPusherForDB(val dbobject: DB) extends InputPusher{
             dbobject.set(sensorData)
       }  
     }
+    log.debug("Successfully saved InfoItems to DB")
   } 
   
   /** Function for handling sequences of path and value pairs.
     *
     */
-  override def handlePathValuePairs( pairs: Seq[(String,TimedValue)] ) : Unit ={
-    dbobject.setMany(pairs.toList)
+  override def handlePathValuePairs( pairs: Seq[(Path,TimedValue)] ) : Unit ={
+    dbobject.setMany(pairs.map{p => (p._1.toString, p._2)}.toList)
+    log.debug("Successfully saved Path-TimedValue pairs to DB")    
   }
   def handlePathMetaDataPairs( pairs: Seq[(Path,String)] ): Unit ={
     pairs.foreach{ pair => dbobject.setMetaData(pair._1, pair._2)}
+    log.debug("Successfully saved Path-MetaData pairs to DB")    
   }
 
 }
