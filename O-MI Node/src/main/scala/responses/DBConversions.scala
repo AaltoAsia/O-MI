1d37c4d23df5ba3b8482f63f8cb653f2f31de83d
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-05-20 12:14:04 +0300

    Small fix for chechking Objects at start of Path's.

--- O-MI Node/src/main/scala/responses/DBConversions.scala
+++ O-MI Node/src/main/scala/responses/DBConversions.scala
@@ -69,6 +69,9 @@ object DBConversion {
     )
   }
   def sensorsToObjects( objects: Array[DBSensor] )(implicit dbConnection: DB) : ObjectsType ={
+    val path = objects.head.path
+    if(objects.contains{sen : DBSensor => sen.path.head != "Objects"})
+      throw new Exception("Different root nodes in InfoItem generation")
     val subobjs = objects.map{
       sobj : DBSensor => 
         DBSensor( sobj.path.tail,  sobj.value, sobj.time )
