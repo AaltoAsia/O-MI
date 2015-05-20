af63b2af7af3ffb1df316dbdb8d1b447fc71d5ab
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-05-20 12:11:53 +0300

    Conversoinsfrom DBSensors to XML generated classes, need to refactor for MetaData, rekursion parameter?

---
+++ O-MI Node/src/main/scala/responses/DBConversions.scala
@@ -0,0 +1,86 @@
+package response
+
+import database._
+import parsing.xmlGen._
+import parsing.Types.Path
+import xml.XML
+
+object DBConversion {
+  def sensorToInfoItem(sensor: Array[DBSensor], metaData : Option[String] = None) : InfoItemType = {
+    val path = sensor.head.path
+    if(sensor.contains{sen : DBSensor => sen.path != path})
+      throw new Exception("Different paths in InfoItem generation")
+    
+    InfoItemType(
+      name = path.last,
+      value = sensor.map{ value : DBSensor =>
+      ValueType(
+        value.value,
+        "",
+        unixTime = Some(value.time.getTime/1000),
+        attributes = Map.empty
+      )
+      },
+      MetaData = 
+        if(metaData.nonEmpty)
+          Some( scalaxb.fromXML[MetaData]( XML.loadString( metaData.get ) ) )
+        else 
+          None
+      ,
+      attributes = Map.empty
+    )
+  }
+  def sensorsToObject(obj: Array[DBSensor] )(implicit dbConnection: DB) : ObjectType = {
+    val path = obj.head.path
+    if(obj.contains{sen : DBSensor => sen.path.head != path.head})
+      throw new Exception("Different root nodes in InfoItem generation")
+    val sensors = obj.filter{ 
+      sensor : DBSensor =>
+        sensor.path.tail.length == 1
+    }.groupBy{
+      sensor : DBSensor =>
+        sensor.path
+    }
+    val subobjs = obj.filter{
+      sensor : DBSensor => 
+        sensor.path.length > 2
+    }.map{
+      sobj : DBSensor => 
+        DBSensor( sobj.path.tail,  sobj.value, sobj.time )
+    }.groupBy{
+      sobj : DBSensor => 
+       sobj.path.head
+    }
+
+    ObjectType(
+      Seq( QlmID(
+        path.head,
+        attributes = Map.empty
+      )),
+    InfoItem= sensors.map{
+      case ( path :  Path, sensor :Array[DBSensor] ) => 
+      sensorToInfoItem(sensor)  
+    }.toSeq,
+    Object = subobjs.map{
+      case ( path :  String, sensors :Array[DBSensor] ) => 
+      sensorsToObject(sensors)
+    }.toSeq,
+      attributes = Map.empty
+    )
+  }
+  def sensorsToObjects( objects: Array[DBSensor] )(implicit dbConnection: DB) : ObjectsType ={
+    val subobjs = objects.map{
+      sobj : DBSensor => 
+        DBSensor( sobj.path.tail,  sobj.value, sobj.time )
+    }.groupBy{
+      sobj : DBSensor => 
+       sobj.path.head
+    }
+    ObjectsType(
+      Object = subobjs.map{
+        case ( path :  String, sensors :Array[DBSensor] ) => 
+        sensorsToObject(sensors)
+      }.toSeq
+    )
+  }
+}
