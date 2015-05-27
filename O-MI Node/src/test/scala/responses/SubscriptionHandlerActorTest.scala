ab508b3f1ac8735f6805a7f0aa4f9105ced68902
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-05-27 14:04:07 +0300

    Test fixes, System test fails

--- O-MI Node/src/test/scala/responses/SubscriptionHandlerActorTest.scala
+++ O-MI Node/src/test/scala/responses/SubscriptionHandlerActorTest.scala
@@ -1,10 +1,12 @@
 package responses
 
+import parsing.Types.Path
+import parsing.Types.OdfTypes._ 
+import parsing.Types.OmiTypes._
 import org.specs2.mutable._
 import akka.testkit.{ TestKit, TestActorRef, TestProbe }
 import akka.actor._
 import database._
-import parsing.Types.Path
 import scala.concurrent._
 import akka.pattern.ask
 import akka.util.Timeout
@@ -26,9 +28,33 @@ class SubscriptionHandlerActorTest extends Specification {
 
   "SubscriptionHandlerActor" should {
     val testPath = Path("SubscriptionHandlerTest/testData")
-
-    val testSub1 = new DBSub(Array(testPath), 60, 2, Some("test"), None)
-    val testSub2 = new DBSub(Array(testPath), 60, -1, Some("test"), None)
+    val odf = OdfObjects(
+      Iterable(
+        OdfObject(
+          Path(testPath.head),
+          Iterable(
+            OdfInfoItem(
+              testPath,
+              Iterable.empty[OdfValue]
+            )
+          ),
+          Iterable.empty[OdfObject]
+        )
+      )
+    )
+    
+    val testSub1 = SubscriptionRequest(
+        60,
+        2,
+        odf,
+        callback = Some("test")
+      )
+    val testSub2 = SubscriptionRequest(
+        60,
+        -1,
+        odf,
+        callback = Some("test")
+      )
 
     val testId1 = Promise[Int]
     val testId2 = Promise[Int]
@@ -51,34 +77,30 @@ class SubscriptionHandlerActorTest extends Specification {
       val subscriptionHandler = TestActorRef[SubscriptionHandler]
       val subscriptionActor = subscriptionHandler.underlyingActor
       val probe = TestProbe()
+      val duration = scala.concurrent.duration.Duration(1000, "ms")
       //      testId.future
 
-      testId1.success(dbConnection.saveSub(testSub1))
-
-      val futureId: Int = Await.result(testId1.future, scala.concurrent.duration.Duration(1000, "ms"))
+      subscriptionHandler.tell(NewSubscription(testSub1), probe.ref)
       //      subscriptionActor.eventSubs.isEmpty === true
-      subscriptionActor.getIntervalSubs.exists(_.id == futureId) === false
-      subscriptionHandler.tell(NewSubscription(futureId), probe.ref)
 
-      subscriptionActor.getIntervalSubs.exists(_.id == futureId) === true
-      subscriptionHandler.tell(RemoveSubscription(futureId), probe.ref)
+      probe.expectMsgType[Int](Duration.apply(2400, "ms")) === 2
+      subscriptionActor.getIntervalSubs.exists(_.id == 2) === true
+      subscriptionHandler.tell(RemoveSubscription(2), probe.ref)
       probe.expectMsgType[Boolean](Duration.apply(2400, "ms")) === true
-      subscriptionActor.getIntervalSubs.exists(_.id == futureId) === false
+      subscriptionActor.getIntervalSubs.exists(_.id == 2) === false
     }
 
     "load given event subs into memory when sent load message" in new Actors {
+      subscriptionActor.getEventSubs.exists(_._2.id == 2) == false
       val subscriptionHandler = TestActorRef[SubscriptionHandler]
       val subscriptionActor = subscriptionHandler.underlyingActor
       val probe = TestProbe()
+      val duration = scala.concurrent.duration.Duration(1000, "ms")
 
-      testId2.success(dbConnection.saveSub(testSub2))
+      subscriptionHandler.tell(NewSubscription(testSub1), probe.ref)
 
-      val futureId: Int = Await.result(testId2.future, scala.concurrent.duration.Duration(1000, "ms"))
-
-      subscriptionActor.getEventSubs.exists(_._2.id == futureId) == false
-      subscriptionHandler.tell(NewSubscription(futureId), probe.ref)
-
-      subscriptionActor.getEventSubs.exists(_._2.id == futureId) === true
+      probe.expectMsgType[Int](Duration.apply(2400, "ms")) === 2
+      subscriptionActor.getIntervalSubs.exists(_.id == 2) === true
 
     }
 
@@ -103,12 +125,10 @@ class SubscriptionHandlerActorTest extends Specification {
       val subscriptionActor = subscriptionHandler.underlyingActor
       val probe = TestProbe()
 
-      val futureId: Int = Await.result(testId2.future, scala.concurrent.duration.Duration(1000, "ms"))
-
-      subscriptionActor.getEventSubs.exists(_._2.id == futureId) === true
-      subscriptionHandler.tell(RemoveSubscription(futureId), probe.ref)
+      subscriptionActor.getEventSubs.exists(_._2.id == 2) === true
+      subscriptionHandler.tell(RemoveSubscription(2), probe.ref)
       probe.expectMsgType[Boolean](Duration.apply(2400, "ms")) === true
-      subscriptionActor.getEventSubs.exists(_._2.id == futureId) === false
+      subscriptionActor.getEventSubs.exists(_._2.id == 2) === false
     }
   }
 }
