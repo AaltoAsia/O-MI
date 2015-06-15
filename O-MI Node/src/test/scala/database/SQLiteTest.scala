5fbc4d4215e09a9993fb71789a887b3c2baaad2c
Jussi Pirilä <jussi.pirila@aalto.fi>
2015-06-15 16:05:24 +0300

    test fix for sqlitetest

--- O-MI Node/src/test/scala/database/SQLiteTest.scala
+++ O-MI Node/src/test/scala/database/SQLiteTest.scala
@@ -6,15 +6,16 @@ import java.sql.Timestamp
 import parsing.Types.OdfTypes._
 import scala.collection.JavaConversions.iterableAsScalaIterable
 import scala.collection.JavaConversions.asJavaIterable
+//import scala.collection.JavaConversions.i
 import testHelpers.AfterAll
 
 import parsing.Types._
 
 object SQLiteTest extends Specification with AfterAll {
 
-  implicit def javaIterableToSeq[T](x: java.lang.Iterable[T]): Seq[T] = {
-    iterableAsScalaIterable(x).toSeq
-  }
+//  implicit def javaIterableToSeq[T](x: java.lang.Iterable[T]): Seq[T] = {
+//    iterableAsScalaIterable(x).toSeq
+//  }
   implicit def seqAsjavaIterable[T](x: Seq[T]): java.lang.Iterable[T] = {
     asJavaIterable(x.toIterable)
   }
@@ -27,14 +28,16 @@ object SQLiteTest extends Specification with AfterAll {
     Iterable(OdfInfoItem(x, Iterable()))
 
   }
-  def OdfObjectsToValues(x: OdfObjects): Option[Seq[String]] = {
-    val temp1: Seq[OdfObject] = x.objects
-    val temp2: Option[Seq[OdfInfoItem]] = temp1.headOption.map(_.infoItems)
-    val temp3: Option[Seq[OdfValue]] = temp2.flatMap(_.headOption.map(_.values))
-    temp3.map(x => x.map(_.value))
+  def OdfObjectsToValues(x: OdfObjects): Seq[String] = {
+    val temp1 = getLeafs(x)
+    val temp = temp1.toSeq.collect{case c: OdfInfoItem => c.values}.flatten.map(_.value)
+    temp
+//    val temp2: Option[Seq[OdfInfoItem]] = temp1.headOption.map(_.infoItems)
+//    val temp3: Option[Seq[OdfValue]] = temp2.flatMap(_.headOption.map(_.values))
+//    temp3.map(x => x.map(_.value))
   }
   def OdfObjectsToPaths(x: OdfObjects): Seq[Path] = {
-    val temp1: Seq[OdfObject] = x.objects
+    val temp1: Seq[OdfObject] = x.objects.toSeq
     temp1.map(n => n.path)
   }
   val testSub1 = db.saveSub(NewDBSub(-1, newTs, -1, None), Array(Path("/Objects/path/to/sensor3/temp")))
@@ -95,7 +98,7 @@ object SQLiteTest extends Specification with AfterAll {
     "return correct childs for given valid path" in {
       db.get(Path("/Objects/path/to/sensor1")) must beSome.like {
         case ob: OdfObject => {
-          val infoitems: Seq[OdfInfoItem] = ob.infoItems
+          val infoitems: Seq[OdfInfoItem] = ob.infoItems.toSeq
           infoitems must have size (2)
           val paths = infoitems.map(n => n.path)
           paths must contain(Path("/Objects/path/to/sensor1/temp"), Path("/Objects/path/to/sensor1/hum"))
@@ -109,9 +112,12 @@ object SQLiteTest extends Specification with AfterAll {
 
     "return correct values for given valid path and timestamps" in {
       val sensors1 = db.getNBetween(pathToInfoItemIterable(Path("/Objects/path/to/sensor1/temp")), Some(new Timestamp(900)), Some(new Timestamp(5500)), None, None) //.getNBetween(Iterable(OdfInfoItem(Path("/Objects/path/to/sensor1/temp")), ), Some(new Timestamp(900)), Some(new Timestamp(5500)), None, None)
-      val values1: Option[Seq[String]] = sensors1.flatMap { x => OdfObjectsToValues(x) }
+      println("DEBUGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG")
+      println(sensors1)
+      println("ASDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
+      val values1: Option[Seq[String]] = sensors1.map { x => OdfObjectsToValues(x) }
       val sensors2 = db.getNBetween(pathToInfoItemIterable(Path("/Objects/path/to/sensor1/temp")), Some(new Timestamp(1500)), Some(new Timestamp(6001)), None, None)
-      val values2: Option[Seq[String]] = sensors2.flatMap { x => OdfObjectsToValues(x) }
+      val values2: Option[Seq[String]] = sensors2.map { x => OdfObjectsToValues(x) }
 
       values1 must beSome.which(_ must have size (2)) // must have size (2)
       values1 must beSome.which(_ must contain("21.5C", "21.6C"))
@@ -122,9 +128,9 @@ object SQLiteTest extends Specification with AfterAll {
 
     "return correct values for N latest values" in {
       val sensors1 = db.getNBetween(pathToInfoItemIterable(Path("/Objects/path/to/sensor3/temp")), None, None, None, Some(12))
-      val values1: Option[Seq[String]] = sensors1.flatMap { x => OdfObjectsToValues(x) }
+      val values1: Option[Seq[String]] = sensors1.map { x => OdfObjectsToValues(x) }
       val sensors2 = db.getNBetween(pathToInfoItemIterable(Path("/Objects/path/to/sensor3/temp")), None, None, None, Some(3))
-      val values2: Option[Seq[String]] = sensors2.flatMap { x => OdfObjectsToValues(x) }
+      val values2: Option[Seq[String]] = sensors2.map { x => OdfObjectsToValues(x) }
 
       values1 must beSome.which(_ must have size (10))
       values1 must beSome.which(_ must contain("21.1C", "21.6C"))
@@ -135,9 +141,9 @@ object SQLiteTest extends Specification with AfterAll {
 
     "return correct values for N oldest values" in {
       val sensors1 = db.getNBetween(pathToInfoItemIterable(Path("/Objects/path/to/sensor3/temp")), None, None, Some(12), None)
-      val values1: Option[Seq[String]] = sensors1.flatMap { x => OdfObjectsToValues(x) }
+      val values1: Option[Seq[String]] = sensors1.map { x => OdfObjectsToValues(x) }
       val sensors2 = db.getNBetween(pathToInfoItemIterable(Path("/Objects/path/to/sensor3/temp")), None, None, Some(2), None)
-      val values2: Option[Seq[String]] = sensors2.flatMap { x => OdfObjectsToValues(x) }
+      val values2: Option[Seq[String]] = sensors2.map { x => OdfObjectsToValues(x) }
 
       values1 must beSome.which(_ must have size (10))
       values1 must beSome.which(_ must contain("21.1C", "21.6C"))
@@ -188,7 +194,7 @@ object SQLiteTest extends Specification with AfterAll {
         None,
         None,
         None)
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beSome.which(_ must have size (21))
 
     }
@@ -200,7 +206,7 @@ object SQLiteTest extends Specification with AfterAll {
         Some(new Timestamp(10000)),
         None,
         None)
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beSome.which(_ must have size (5))
 
       val temp3 = db.getNBetween(
@@ -209,7 +215,7 @@ object SQLiteTest extends Specification with AfterAll {
         Some(new Timestamp(10000)),
         Some(10),
         None)
-      val temp4 = temp3.flatMap(OdfObjectsToValues(_))
+      val temp4 = temp3.map(OdfObjectsToValues(_))
       temp4 must beSome.which(_ must have size (5))
     }
 
@@ -220,7 +226,7 @@ object SQLiteTest extends Specification with AfterAll {
         None,
         None,
         None)
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beSome.which(_ must have size (7))
     }
 
@@ -231,7 +237,7 @@ object SQLiteTest extends Specification with AfterAll {
         Some(new Timestamp(10000)),
         None,
         None)
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beSome.which(_ must have size (5))
     }
 
@@ -242,7 +248,7 @@ object SQLiteTest extends Specification with AfterAll {
         Some(new Timestamp(26000)),
         None,
         Some(10))
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beSome.which(_ must have size (10))
     }
 
@@ -253,7 +259,7 @@ object SQLiteTest extends Specification with AfterAll {
         None,
         Some(10),
         None)
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beSome.which(_ must have size (10))
     }
 
@@ -264,7 +270,7 @@ object SQLiteTest extends Specification with AfterAll {
         None,
         None,
         None)
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beSome.which(_ must have size (21))
     }
 
@@ -275,7 +281,7 @@ object SQLiteTest extends Specification with AfterAll {
         None,
         Some(10),
         Some(5))
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beSome.which(_ must have size (21))
     }
 
@@ -286,7 +292,7 @@ object SQLiteTest extends Specification with AfterAll {
         None,
         None,
         None)
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beNone
     }
 
@@ -299,7 +305,7 @@ object SQLiteTest extends Specification with AfterAll {
         None,
         None,
         None)
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beSome.which(_ must have size (21))
     }
 
@@ -311,7 +317,7 @@ object SQLiteTest extends Specification with AfterAll {
         None,
         None,
         None)
-      val temp2 = temp1.flatMap(OdfObjectsToValues(_))
+      val temp2 = temp1.map(OdfObjectsToValues(_))
       temp2 must beSome.which(_ must have size (10))
     }
 
@@ -436,10 +442,10 @@ object SQLiteTest extends Specification with AfterAll {
       val pathValuePairs = testdata.map(n => (Path(n._1), n._2))
       db.setMany(pathValuePairs)
       val temp1 = db.getNBetween(pathToInfoItemIterable(Path("/Objects/path/to/setmany/test1")), None, None, None, None)
-      val values1 = temp1.flatMap(OdfObjectsToValues(_))
+      val values1 = temp1.map(OdfObjectsToValues(_))
       values1 must beSome.which(_ must have size (12))
       val temp2 = db.getNBetween(pathToInfoItemIterable(Path("/Objects/path/to/setmany/test2")), None, None, None, None)
-      val values2 = temp1.flatMap(OdfObjectsToValues(_))
+      val values2 = temp1.map(OdfObjectsToValues(_))
       values2 must beSome.which(_ must have size (10))
       db.removeSub(testSub3.id)
       db.remove(Path("/Objects/path/to/setmany/test1"))
