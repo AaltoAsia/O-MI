a56eedda32c6bd5e18b8ce65c511c45e4705c2fe
Jussi Pirilä <jussi.pirila@aalto.fi>
2015-06-25 16:31:14 +0300

    structure for systemtests

--- O-MI Node/src/test/scala/http/SystemTest.scala
+++ O-MI Node/src/test/scala/http/SystemTest.scala
@@ -1,27 +1,27 @@
-//package http
-//import org.specs2.mutable.Specification
-//import spray.http._
-//import spray.client.pipelining._
-//import akka.actor.ActorSystem
-//import scala.concurrent._
-//
-//class SystemTest extends Specification{
-//  
-//  implicit val system = ActorSystem()
-//  import system.dispatcher
-//  val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
-//  val printer = new scala.xml.PrettyPrinter(80, 2)
-//
-//  
-//  "test block" should {
-//    (1 to 500) foreach { i=>
-//      ("example " + i ) in {(i % 2 === 0)}.updateMessage(f => "asd")
-//      }
-//    
-//    (500 to 1000 by 2) foreach { i=>
-//      ("example " + i ) >> {
-//        (i aka "lol failed" must_== 1000)}
-//      }
-//    
-//  }
-//}
+package http
+import org.specs2.mutable.Specification
+import spray.http._
+import spray.client.pipelining._
+import akka.actor.ActorSystem
+import scala.concurrent._
+
+class SystemTest extends Specification{
+  
+  implicit val system = ActorSystem()
+  import system.dispatcher
+  val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
+  val printer = new scala.xml.PrettyPrinter(80, 2)
+
+  
+  "test block" should {
+    (1 to 500) foreach { i=>
+      ("example " + i ) in {(i % 2 === 0)}.updateMessage(f => "asd")
+      }
+    
+    (500 to 1000 by 2) foreach { i=>
+      ("example " + i ) >> {
+        (i aka "lol failed" must_== 1000)}
+      }
+    
+  }
+}
