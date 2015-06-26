c809a162ce9ab2c6b696c704efbfd59f72738383
Kinnunen Tuomas <tuomas.kinnunen@aalto.fi>
2015-06-26 12:10:28 +0300

    fix cors support and updated js examples

--- O-MI Node/src/main/scala/http/OmiService.scala
+++ O-MI Node/src/main/scala/http/OmiService.scala
@@ -49,16 +49,12 @@ class OmiServiceActor(reqHandler: RequestHandler) extends Actor with ActorLoggin
 /**
  * this trait defines our service behavior independently from the service actor
  */
-trait OmiService extends HttpService {
+trait OmiService extends HttpService with CORSSupport {
   import scala.concurrent.ExecutionContext.Implicits.global
   def log: LoggingAdapter
   val requestHandler: RequestHandler
 
   implicit val dbobject: DB
-
-    //Handles CORS allow-origin seems to be enough
-  private def corsHeaders =
-    respondWithHeader(RawHeader("Access-Control-Allow-Origin", "*"))
 
   //Get the files from the html directory; http://localhost:8080/html/form.html
   val staticHtml = getFromDirectory("html")
@@ -73,23 +69,21 @@ trait OmiService extends HttpService {
 
   // should be removed?
   val helloWorld = get {
-    corsHeaders {
-      respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default
-        complete {
-          <html>
-            <body>
-              <h1>Say hello to <i>O-MI Node service</i>!</h1>
-              <a href="Objects">Url Data Discovery /Objects: Root of the hierarchy</a>
-              <p>
-                With url data discovery you can discover or request Objects,
-                 InfoItems and values with HTTP Get request by giving some existing
-                 path to the O-DF xml hierarchy.
-              </p>
-              <a href="html/form.html">O-MI Test Client WebApp</a><br/>
-              <a href="html/ImplementationDetails.html">Implementation details, request-response examples</a>
-            </body>
-          </html>
-        }
+    respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default
+      complete {
+        <html>
+          <body>
+            <h1>Say hello to <i>O-MI Node service</i>!</h1>
+            <a href="Objects">Url Data Discovery /Objects: Root of the hierarchy</a>
+            <p>
+              With url data discovery you can discover or request Objects,
+               InfoItems and values with HTTP Get request by giving some existing
+               path to the O-DF xml hierarchy.
+            </p>
+            <a href="html/form.html">O-MI Test Client WebApp</a><br/>
+            <a href="html/ImplementationDetails.html">Implementation details, request-response examples</a>
+          </body>
+        </html>
       }
     }
   }
@@ -101,22 +95,20 @@ trait OmiService extends HttpService {
         val pathStr = pathToString(sprayPath)
         val path = Path(pathStr)
 
-        corsHeaders {
-          requestHandler.generateODFREST(path) match {
-            case Some(Left(value)) =>
-              respondWithMediaType(`text/plain`) {
-                complete(value)
-              }
-            case Some(Right(xmlData)) =>
-              respondWithMediaType(`text/xml`) {
-                complete(xmlData)
-              }
-            case None =>
-              log.debug(s"Url Discovery fail: org: [$pathStr] parsed: [$path]")
-              respondWithMediaType(`text/xml`) {
-                complete((404, <error>No object found</error>))
-              }
-          }
+        requestHandler.generateODFREST(path) match {
+          case Some(Left(value)) =>
+            respondWithMediaType(`text/plain`) {
+              complete(value)
+            }
+          case Some(Right(xmlData)) =>
+            respondWithMediaType(`text/xml`) {
+              complete(xmlData)
+            }
+          case None =>
+            log.debug(s"Url Discovery fail: org: [$pathStr] parsed: [$path]")
+            respondWithMediaType(`text/xml`) {
+              complete((404, <error>No object found</error>))
+            }
         }
       }
     }
@@ -124,38 +116,36 @@ trait OmiService extends HttpService {
   /* Receives HTTP-POST directed to root */
   val postXMLRequest = post { // Handle POST requests from the client
     clientIP { ip => // XXX: NOTE: This will fail if there isn't setting "remote-address-header = on"
-      corsHeaders {
-        entity(as[NodeSeq]) { xml =>
-          val eitherOmi = OmiParser.parse(xml.toString)
-          //lazy val ip: RemoteAddress = ???
+      entity(as[NodeSeq]) { xml =>
+        val eitherOmi = OmiParser.parse(xml.toString)
+        //lazy val ip: RemoteAddress = ???
 
-          respondWithMediaType(`text/xml`) {
-            eitherOmi match {
-              case Right(requests) =>
-                val request = requests.head
+        respondWithMediaType(`text/xml`) {
+          eitherOmi match {
+            case Right(requests) =>
+              val request = requests.head
 
-                val (response, returnCode) = request match {
+              val (response, returnCode) = request match {
 
-                  case pRequest : PermissiveRequest => 
-                    if(ip.toOption.nonEmpty && hasPermission(ip.toOption.get))
-                      requestHandler.handleRequest(pRequest)
-                    else
-                      (requestHandler.unauthorized, 401)
+                case pRequest : PermissiveRequest => 
+                  if(ip.toOption.nonEmpty && hasPermission(ip.toOption.get))
+                    requestHandler.handleRequest(pRequest)
+                  else
+                    (requestHandler.unauthorized, 401)
 
-                  case req : OmiRequest => 
-                      requestHandler.handleRequest(request)
-                }
+                case req : OmiRequest => 
+                    requestHandler.handleRequest(request)
+              }
 
-                complete((returnCode, response))
+              complete((returnCode, response))
 
-              case Left(errors) =>  // Errors found
+            case Left(errors) =>  // Errors found
 
-                log.warning("Parse Errors: {}", errors.mkString(", "))
+              log.warning("Parse Errors: {}", errors.mkString(", "))
 
-                val errorResponse = requestHandler.parseError(errors.toSeq:_*)
+              val errorResponse = requestHandler.parseError(errors.toSeq:_*)
 
-                complete((400, errorResponse))
-            }
+              complete((400, errorResponse))
           }
         }
       }
@@ -163,7 +153,8 @@ trait OmiService extends HttpService {
   }
 
   // Combine all handlers
-  val myRoute = path("") {
+  val myRoute = cors {
+    path("") {
       postXMLRequest ~
       helloWorld
     } ~
@@ -173,4 +164,5 @@ trait OmiService extends HttpService {
     pathPrefixTest("Objects") {
       getDataDiscovery
     }
+  }
 }
