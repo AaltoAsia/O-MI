2844d731e9e436b17b5bf88b81192ed9173395cc
Jussi Pirilä <jussi.pirila@aalto.fi>
2015-06-26 16:31:34 +0300

    tests now work but fail

--- O-MI Node/src/test/scala/http/SystemTest.scala
+++ O-MI Node/src/test/scala/http/SystemTest.scala
@@ -1,6 +1,7 @@
 package http
 
-import org.specs2.mutable.Specification
+import org.specs2.mutable._//Specification
+import org.specs2.matcher.XmlMatchers._
 import org.xml.sax.InputSource
 import spray.http._
 import spray.client.pipelining._
@@ -13,38 +14,56 @@ import testHelpers.HTML5Parser
 
 
 class SystemTest extends Specification{
-
-
-  
-  
+sequential
   implicit val system = ActorSystem()
   import system.dispatcher
-  val pipeline: HttpRequest => Future[String] = sendReceive
+  
+  val pipeline: HttpRequest => Future[NodeSeq] = sendReceive ~> unmarshal[NodeSeq]
   val printer = new scala.xml.PrettyPrinter(80, 2)
   
   val parser = new HTML5Parser
   val sourceFile = Source.fromFile("html/ImplementationDetails.html")
   val sourceXML: Node = parser.loadXML(sourceFile)
+  val testArticles = sourceXML \\ ("article")
+  val tests = testArticles.groupBy( x => x.\@("class") )
   
-  println("WWWWWWWWWWWWWWWWWWW")
-  val test1 = sourceXML.\\("textarea").tail
-  val test2 = test1.head.text//.filter(_.\@("class") == "test").head.\("textarea")
-  val test3 = test1.tail.head.text
-  println(test2)
-  println(test3)
+  val readTests = tests("read test").map { node => 
+    val textAreas = node \\ ("textarea")
+    val request = Try(textAreas.find(x=> x.\@("class") == "request").map(_.text).map(XML.loadString(_))).toOption.flatten
+    val correctResponse = Try(textAreas.find(x => x.\@("class") == "response").map(_.text).map(XML.loadString(_))).toOption.flatten
+    val description = node \ ("div") \ ("p") text
+    
+    (request, correctResponse, description)
+    }//.filter(a => a.\@("class") == "read test").map(_ \\ ("textarea")).map(p => p)
   
-  
-  "test block" should {
+  "Automatic System Tests" should {
     "WriteTest" in {
-      println("ASDAKLSDKALSKD")
-      val response = pipeline(Post("http://localhost:8080/", test1))
-      val waited = Await.result(response, scala.concurrent.duration.Duration.apply(5, "second"))
-      println()
-      println(waited)
-      waited.entity.asString must be(test3)
+      //Only 1 write test
+      val testCase = tests("write test").head \\ ("textarea")
+      //val testCase = testArticles.filter(a => a.\@("class") == "write test" ).head \\ ("textarea") //sourceXML \\ ("write test")
+      val request: Option[Elem] = Try(testCase.find(x=> x.\@("class") == "request").map(_.text).map(XML.loadString(_))).toOption.flatten
+      val correctResponse: Option[Elem] = Try(testCase.find(x => x.\@("class") == "response").map(_.text).map(XML.loadString(_))).toOption.flatten
+      
+      request aka "Write request message" must beSome
+      correctResponse aka "Correct write response message" must beSome
+      
+      val response = pipeline(Post("http://localhost:8080/", request.get))
+      
+      response must beEqualToIgnoringSpace(correctResponse.get).await(2, scala.concurrent.duration.Duration.apply(2, "second"))
     }
-    (1 to 10) foreach { i=>
-      ("example " + i ) in {(i % 2 === 0).updateMessage(f=> "ASDASD")}.updateMessage(f => "asd")
+    
+    readTests foreach { i=>
+      val(request, correctResponse, description) = i
+      ("test Case:\n" + description.trim ) >> {
+        
+        request aka "Read request message" must beSome
+        correctResponse aka "Correct read response message" must beSome
+        
+        val response = pipeline(Post("http://localhost:8080/", request.get))
+        
+        response must beEqualToIgnoringSpace(correctResponse.get).await(2, scala.concurrent.duration.Duration.apply(2, "second"))
+        }
+        //(i % 2 === 0).updateMessage(f=> "ASDASD")}.updateMessage(f => "asd")
       }
     //Example to change test failure message
     /*
