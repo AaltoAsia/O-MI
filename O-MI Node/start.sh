3eacda1cb9bf8c3397fb5d683d137f4bee534fcd
Kinnunen Tuomas <tuomas.kinnunen@aalto.fi>
2015-06-30 15:51:28 +0300

    new starting script

--- O-MI Node/start.sh
+++ O-MI Node/start.sh
@@ -1,3 +1,37 @@
-#!/bin/sh
-java -jar -Dconfig.file=application.conf o-mi-node-0.1-SNAPSHOT.jar
+#!/bin/bash
+# Usage
+# * To start: ./start.sh
+# * To stop:  ./start.sh stop
+
+pidfile=./o-mi-node.pid
+
+if [[ $1 == "-h" || $1 == "--help" ]]; then
+    echo "Usage"
+    echo "* To start: ./start.sh"
+    echo "* To stop:  ./start.sh stop"
+fi
+
+# pid check
+if [[ -f $pidfile ]]; then
+    oldpid=`cat $pidfile`
+    if ps ax | awk '{print $1}' | grep $oldpid > /dev/null; then
+        echo Stopping pid $oldpid...
+        kill $oldpid
+    fi
+    rm $pidfile
+fi
+
+if [[ $1 == "stop" ]]; then
+    exit 0
+fi
+
+
+echo Starting...
+echo java -jar -Dconfig.file=application.conf o-mi-node-*.jar >> "log-startedon-`date -I`" &
+javapid=$!
+echo Started pid $javapid.
+
+# save pid
+echo $javapid > $pidfile
+
 
