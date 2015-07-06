1a306b315c7aa9aee35e2466ec0d972e3ad8109c
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-07-06 11:02:57 +0300

    Futureclient GUI changed

--- O-MI Node/FutureClient/js/myjs.js
+++ O-MI Node/FutureClient/js/myjs.js
@@ -1,10 +1,15 @@
-
-$(function () { $('#nodetree').jstree({"plugins" : ["checkbox"]}); });
-$('#jstree_demo_div').on("changed.jstree", function (e, data) {
+$( function(){
+$('#nodetree').jstree({"plugins" : ["checkbox"]}); 
+$('#requesttree').jstree(); 
+$('#nodetree').on("changed.jstree", function (e, data) {
+      console.log(data.selected);
+});
+$('#requesttree').on("changed.jstree", function (e, data) {
       console.log(data.selected);
 });
 $('button').on('click', function () {
       $('#jstree').jstree(true).select_node('child_node_1');
         $('#jstree').jstree('select_node', 'child_node_1');
 	  $.jstree.reference('#jstree').select_node('child_node_1');
+});
 });
