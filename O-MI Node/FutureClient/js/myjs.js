a56ca23645bb83768c49cde67b5bb5e0d6e5be97
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-07-06 14:31:49 +0300

    Code mirror added.

--- O-MI Node/FutureClient/js/myjs.js
+++ O-MI Node/FutureClient/js/myjs.js
@@ -1,4 +1,6 @@
 $( function(){
+
+
 $('#nodetree').jstree({"plugins" : ["checkbox"]}); 
 $('#requesttree').jstree(); 
 $('#nodetree').on("changed.jstree", function (e, data) {
@@ -12,4 +14,16 @@ $('button').on('click', function () {
         $('#jstree').jstree('select_node', 'child_node_1');
 	  $.jstree.reference('#jstree').select_node('child_node_1');
 });
+
+});
+var requestCodeMirror = CodeMirror.fromTextArea(document.getElementById("requestArea"), {
+    mode: "text/html",
+    lineNumbers: true,
+    lineWrapping: true
+});
+
+var responseCodeMirror = CodeMirror.fromTextArea(document.getElementById("responseArea"), {
+    mode: "text/html",
+    lineNumbers: true,
+    lineWrapping: true
 });
