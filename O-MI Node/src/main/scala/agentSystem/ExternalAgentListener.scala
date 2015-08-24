48ecf0ff13b8396ec4ca5e87dae613549ee68586
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-08-24 10:25:10 +0300

    4-clause BSD LICENSE added.

--- O-MI Node/src/main/scala/agentSystem/ExternalAgentListener.scala
+++ O-MI Node/src/main/scala/agentSystem/ExternalAgentListener.scala
@@ -1,3 +1,18 @@
+/**
+  Copyright (c) 2015 Aalto University.
+
+  Licensed under the 4-clause BSD (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  https://github.com/AaltoAsia/O-MI/blob/master/LICENSE.txt
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+**/
 package agentSystem
 
 import akka.actor.{ Actor, Props, ActorLogging }
