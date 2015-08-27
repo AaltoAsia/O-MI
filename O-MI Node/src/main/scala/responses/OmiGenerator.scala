2ce1fd50d3656f02be6cd86af57effd7bfa7a56f
Isojärvi Lauri <isojarl1@niksula.hut.fi>
2015-08-27 12:53:50 +0300

    Responses pacgakes clean up and documentation. SubscriptionHandler need
    needs documentation, for newest refactor.

--- O-MI Node/src/main/scala/responses/OmiGenerator.scala
+++ O-MI Node/src/main/scala/responses/OmiGenerator.scala
@@ -11,29 +11,53 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 **/
+
 package responses
+package OmiGenerator
 
-
+import types._
+import parsing.xmlGen._
 import parsing.xmlGen.xmlTypes._
 import parsing.xmlGen.scalaxb
+import parsing.xmlGen.defaultScope
 import parsing.xmlGen.scalaxb._
 import scala.xml.NodeSeq
+import scala.language.existentials
 
-/** Object containing helper mehtods for generating scalaxb generated O-MI types. Used to generate response messages when received a request.
-  *
-  **/
-object OmiGenerator {
+/** Package containing helper mehtods for generating scalaxb generated O-MI types.
+  *  Used to generate response messages when received a request.
+  */
+object `package` {
   
+  
+  /** Generates O-MI OmiEnvelope
+    * @param ttl ttl attribute of omiEnvelope
+    * @param requestName name of request: "read", "write", "cancel", "response".
+    * @param request Request with requestName tag as root.
+    * @param version of O-MI
+    * @return O-MI omiEnvelope tag.
+    */
   def omiEnvelope[ R <: OmiEnvelopeOption : CanWriteXML ](ttl: Double, requestName: String, request: R , version: String = "1.0") = {
       OmiEnvelope( DataRecord[R](Some("omi.xsd"), Some(requestName), request), version, ttl)
   }
   
+  /** Generates O-MI ResponseListType
+    * @param results O-MI result tags.
+    * @return O-MI response tag.
+    */
   def omiResponse( results: RequestResultType*) : ResponseListType = {
     ResponseListType(
       results:_*
     )
   }
   
+  /** Generates O-MI RequestResultType.
+    * @param returnType O-MI return tag.
+    * @param requestID O-MI requestID tag's value.
+    * @param msgformat value for omiEnvelope tag's msgformat attribute.
+    * @param msg msg tag.
+    * @return O-MI result tag.
+    */
   def omiResult(returnType: ReturnType, requestID: Option[String] = None, msgformat: Option[String] = None, msg: Option[NodeSeq] = None) : RequestResultType = {
     RequestResultType(
         returnType,
@@ -54,14 +78,111 @@ object OmiGenerator {
       )
   } 
 
+  /** Generates O-MI ReturnType 
+    * @param returnCode HTTP return code.
+    * @param description Description of return code.
+    * @param value Value in omi:return tag.
+    * @return O-MI return tag.
+    */
   def omiReturn( returnCode: String, description: Option[String] = None, value: String = "") : ReturnType={
     ReturnType(value, returnCode, description, attributes = Map.empty)
   }
 
-  def odfMsg( value: NodeSeq )={
+  /** Wraps O-DF format to O-MI msg tag.
+    * @param odf O-DF Structure.
+    * @return O-MI msg tag.
+    */
+  def odfMsg( odf: NodeSeq )={
     <omi:msg xmlns="odf.xsd">
-      {value}
+      {odf}
     </omi:msg>
   }
+
+  /** Generates O-MI response tag from O-MI result tags.
+    * @param ttl attribute for omiEnvelope.
+    * @param results O-MI result tags.
+    * @return O-MI omiEnvelope tag.
+    */
+  def wrapResultsToResponseAndEnvelope(ttl: Double, results: RequestResultType*) = {
+   omiEnvelope(ttl, "response", omiResponse(results: _*))
+  }
+
+  /** Generates OmiEnvelope from O-MI results.
+    * @param ttl in secods as Double.
+    * @param results used in generation.
+    * @return O-MI omiEnvelo tag as xml.NodeSeq.
+    */
+  def xmlFromResults(ttl: Double, results: RequestResultType*) = {
+    xmlMsg(wrapResultsToResponseAndEnvelope(ttl, results: _*))
+  }
+
+  /**
+   * Generates xml from OmiEnvelope
+   *
+   * @param envelope xmlType for OmiEnvelope containing response
+   * @return xml.NodeSeq containing response
+   */
+  def xmlMsg(envelope: OmiEnvelope) = {
+    scalaxb.toXML[OmiEnvelope](envelope, Some("omi.xsd"), Some("omiEnvelope"), defaultScope)
+  }
+   
+  /** O-MI response for not found paths/subscription. */
+  def notFound = xmlFromResults(
+    1.0,
+    Results.notFound)
+  
+  /** O-MI response for successfully handled request with out any other information.*/
+  def success = xmlFromResults(
+    1.0,
+    Results.success)
+  
+  /** O-MI response for request which permission was denied.*/
+  def unauthorized = xmlFromResults(
+    1.0,
+    Results.unauthorized)
+  
+  /** O-MI response for features not implemented.*/
+  def notImplemented = xmlFromResults(
+    1.0,
+    Results.notImplemented)
+  
+  /** O-MI response for invalid request.
+    * @param msg Message defining invalidation.
+    */
+  def invalidRequest(msg: String = "") = xmlFromResults(
+    1.0,
+    Results.invalidRequest(msg))
+  
+  /** O-MI response for request which malformed content.
+    * @param err ParseErrors
+    */
+  def parseError(err: ParseError*) =
+    xmlFromResults(
+      1.0,
+      Results.simple("400",
+        Some(err.map { e => e.msg }.mkString("\n"))))
+
+  /** O-MI response for request which callback address was malformed.
+    * @param callback Invalid callback address
+    */
+  def invalidCallback(callback: String) =
+    xmlFromResults(
+      1.0,
+      Results.simple("400",
+        Some("Invalid callback address: " + callback)))
+
+  /** O-MI response for request that caused an exception.
+    * @param e Exception caught.
+    */
+  def internalError(e: Throwable) =
+    xmlFromResults(
+      1.0,
+      Results.simple("501", Some("Internal server error: " + e.getMessage())))
+
+  /** O-MI response for request which ttl timedout. */
+  def timeOutError = xmlFromResults(
+    1.0,
+    Results.simple("500", Some("TTL timeout, consider increasing TTL or is the server overloaded?")))
 }
 
+import OmiGenerator._
--- O-MI Node/src/main/scala/responses/RequestHandler.scala
+++ O-MI Node/src/main/scala/responses/RequestHandler.scala
@@ -38,8 +38,10 @@ import java.net.{ URL, InetAddress, UnknownHostException }
 import scala.xml.{ NodeSeq, XML }
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import OmiGenerator._
+import parsing.xmlGen.defaultScope
 /**
- * Class for handling all request.
+ * Actor for handling all request.
  *
  */
 class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnection: DB) {
@@ -104,7 +106,7 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
               (
                 xmlFromResults(
                   1.0,
-                  Result.simple("200", Some("OK, callback job started"))),
+                  Results.simple("200", Some("OK, callback job started"))),
                   200)
             }
           }
@@ -144,7 +146,7 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
         (
           xmlFromResults(
             1.0,
-            Result.simple("500", Some("TTL timeout, consider increasing TTL or is the server overloaded?"))),
+            Results.simple("500", Some("TTL timeout, consider increasing TTL or is the server overloaded?"))),
             500)
       case Failure(e: IllegalArgumentException) =>
         (invalidRequest(e.getMessage), 400)
@@ -154,14 +156,14 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
         (
           xmlFromResults(
             1.0,
-            Result.simple(e.errorCode.toString, Some(e.getMessage()))),
+            Results.simple(e.errorCode.toString, Some(e.getMessage()))),
             501)
       case Failure(e) =>
         actionOnInternalError(e)
         (
           xmlFromResults(
             1.0,
-            Result.simple("501", Some("Internal server error: " + e.getMessage()))),
+            Results.simple("501", Some("Internal server error: " + e.getMessage()))),
             501)
     }
   }
@@ -210,7 +212,7 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
               InputPusher.handleObjects(odf.objects)
             case None =>//noop?
           }
-          Result.success
+          Results.success
         }.toSeq:_*
       ),
       200
@@ -223,45 +225,26 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
       handleSubData(subdata)
     }
     case _ => {
-      (xmlFromResults(1.0, Result.simple("500", Some("Unknown request."))), 500)
+      (xmlFromResults(1.0, Results.simple("500", Some("Unknown request."))), 500)
     }
   }
 
-  private[this] val scope = scalaxb.toScope(
-    None -> "odf.xsd",
-    Some("omi") -> "omi.xsd",
-    Some("xs") -> "http://www.w3.org/2001/XMLSchema",
-    Some("xsi") -> "http://www.w3.org/2001/XMLSchema-instance")
-  def wrapResultsToResponseAndEnvelope(ttl: Double, results: xmlTypes.RequestResultType*) = {
-    OmiGenerator.omiEnvelope(ttl, "response", OmiGenerator.omiResponse(results: _*))
-  }
-
-  def xmlFromResults(ttl: Double, results: xmlTypes.RequestResultType*) = {
-    xmlMsg(wrapResultsToResponseAndEnvelope(ttl, results: _*))
-  }
-
-  /**
-   * Generates xml from xmlTypes
-   *
-   * @param envelope xmlType for OmiEnvelope containing response
-   * @return xml.NodeSeq containing response
-   */
-  def xmlMsg(envelope: xmlTypes.OmiEnvelope) = {
-    scalaxb.toXML[xmlTypes.OmiEnvelope](envelope, Some("omi.xsd"), Some("omiEnvelope"), scope)
-  }
-
+  /** Method for handling ReadRequest.
+    * @param read request
+    * @return (xml response, HTTP status code)
+    */
   def handleRead(read: ReadRequest): (NodeSeq, Int) = {
     val objectsO: Option[OdfObjects] = dbConnection.getNBetween(getLeafs(read.odf), read.begin, read.end, read.newest, read.oldest)
 
     objectsO match {
       case Some(objects) =>
-        val found = Result.read(objects)
+        val found = Results.read(objects)
         val requestsPaths = getLeafs(read.odf).map { _.path }
         val foundOdfAsPaths = getLeafs(objects).flatMap { _.path.getParentsAndSelf }.toSet
         val notFound = requestsPaths.filterNot { path => foundOdfAsPaths.contains(path) }.toSet.toSeq
         var results = Seq(found)
         if (notFound.nonEmpty)
-          results ++= Seq(Result.simple("404",
+          results ++= Seq(Results.simple("404",
             Some("Could not find the following elements from the database:\n" + notFound.mkString("\n"))))
 
         (
@@ -271,10 +254,14 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
             200)
       case None =>
         (xmlFromResults(
-          1.0, Result.notFound), 404)
+          1.0, Results.notFound), 404)
     }
   }
 
+  /** Method for handling PollRequest.
+    * @param poll request
+    * @return (xml response, HTTP status code)
+    */
   def handlePoll(poll: PollRequest): (NodeSeq, Int) = {
     val time = date.getTime
     val results =
@@ -284,9 +271,9 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
 
         objectsO match {
           case Some(objects) =>
-            Result.poll(id.toString, objects)
+            Results.poll(id.toString, objects)
           case None =>
-            Result.notFoundSub(id.toString)
+            Results.notFoundSub(id.toString)
         }
       }
     val returnTuple = (
@@ -298,21 +285,25 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
     returnTuple
   }
 
+  /** Method for handling SubscriptionRequest.
+    * @param subscription request
+    * @return (xml response, HTTP status code)
+    */
   def handleSubscription(subscription: SubscriptionRequest): (NodeSeq, Int) = {
     implicit val timeout = Timeout(10.seconds) // NOTE: ttl will timeout from elsewhere
     val subFuture = subscriptionHandler ? NewSubscription(subscription)
     val (response, returnCode) =
       Await.result(subFuture, Duration.Inf) match {
         case Failure(e: IllegalArgumentException) =>
-          (Result.invalidRequest(e.getMessage), 400)
+          (Results.invalidRequest(e.getMessage), 400)
         case Failure(t) =>
-          (Result.internalError(
+          (Results.internalError(
             s"Internal server error when trying to create subscription: ${t.getMessage}"),
             500)
         case Success(id: Long) =>
-          (Result.subscription(id.toString), 200)
+          (Results.subscription(id.toString), 200)
         case Success(received) =>
-          (Result.internalError(
+          (Results.internalError(
             s"Internal server error: Invalid response type from SubscriptionHandler: ${received.getClass().getName}"),
             500)
       }
@@ -323,6 +314,10 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
         returnCode)
   }
 
+  /** Method for handling CancelRequest.
+    * @param cancel request
+    * @return (xml response, HTTP status code)
+    */
   def handleCancel(cancel: CancelRequest): (NodeSeq, Int) = {
     implicit val timeout = Timeout(10.seconds) // NOTE: ttl will timeout from elsewhere
     var returnCode = 200
@@ -339,27 +334,27 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
           case Success(removeFuture) =>
             // NOTE: ttl will timeout from OmiService
             Await.result(removeFuture, Duration.Inf) match {
-              case true => Result.success
+              case true => Results.success
               case false => {
                 returnCode = 404
-                Result.notFoundSub
+                Results.notFoundSub
               }
               case _ => {
                 returnCode = 501
-                Result.internalError()
+                Results.internalError()
               }
             }
           case Failure(n: NumberFormatException) => {
             returnCode = 400
-            Result.simple(returnCode.toString, Some("Invalid requestID"))
+            Results.simple(returnCode.toString, Some("Invalid requestID"))
           }
           case Failure(e: RequestHandlingException) => {
             returnCode = e.errorCode
-            Result.simple(returnCode.toString, Some(e.msg))
+            Results.simple(returnCode.toString, Some(e.msg))
           }
           case Failure(e) => {
             returnCode = 501
-            Result.internalError("Internal server error, when trying to cancel subscription: " + e.toString)
+            Results.internalError("Internal server error, when trying to cancel subscription: " + e.toString)
           }
         }(breakOut): _*),
         returnCode)
@@ -371,47 +366,15 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
     objectsO match {
       case Some(objects) =>
         (xmlFromResults(
-          1.0, Result.poll(subdata.sub.id.toString, objects)), 200)
+          1.0, Results.poll(subdata.sub.id.toString, objects)), 200)
 
       case None =>
         (xmlFromResults(
-          1.0, Result.notFound), 404)
+          1.0, Results.notFound), 404)
     }
 
   }
 
-  def notFound = xmlFromResults(
-    1.0,
-    Result.notFound)
-  def success = xmlFromResults(
-    1.0,
-    Result.success)
-  def unauthorized = xmlFromResults(
-    1.0,
-    Result.unauthorized)
-  def notImplemented = xmlFromResults(
-    1.0,
-    Result.notImplemented)
-  def invalidRequest(msg: String = "") = xmlFromResults(
-    1.0,
-    Result.invalidRequest(msg))
-  def parseError(err: ParseError*) =
-    xmlFromResults(
-      1.0,
-      Result.simple("400",
-        Some(err.map { e => e.msg }.mkString("\n"))))
-  def invalidCallback(err: String) =
-    xmlFromResults(
-      1.0,
-      Result.simple("400",
-        Some("Invalid callback address: " + err)))
-  def internalError(e: Throwable) =
-    xmlFromResults(
-      1.0,
-      Result.simple("501", Some("Internal server error: " + e.getMessage())))
-  def timeOutError = xmlFromResults(
-    1.0,
-    Result.simple("500", Some("TTL timeout, consider increasing TTL or is the server overloaded?")))
   /**
    * Generates ODF containing only children of the specified path's (with path as root)
    * or if path ends with "value" it returns only that value.
@@ -454,16 +417,16 @@ class RequestHandler(val subscriptionHandler: ActorRef)(implicit val dbConnectio
 
           case _ =>
             return Some(Right(
-              scalaxb.toXML[xmlTypes.InfoItemType](infoitem.asInfoItemType, Some("odf"), Some("InfoItem"), scope).headOption.getOrElse(
+              scalaxb.toXML[xmlTypes.InfoItemType](infoitem.asInfoItemType, Some("odf"), Some("InfoItem"), defaultScope).headOption.getOrElse(
                 <error>Could not create from OdfInfoItem </error>)))
         }
       case Some(odfObj: OdfObject) =>
-        val xmlReturn = scalaxb.toXML[xmlTypes.ObjectType](odfObj.asObjectType, Some("odf"), Some("Object"), scope).headOption.getOrElse(
+        val xmlReturn = scalaxb.toXML[xmlTypes.ObjectType](odfObj.asObjectType, Some("odf"), Some("Object"), defaultScope).headOption.getOrElse(
           <error>Could not create from OdfObject </error>)
         Some(Right(xmlReturn))
 
       case Some(odfObj: OdfObjects) =>
-        val xmlReturn = scalaxb.toXML[xmlTypes.ObjectsType](odfObj.asObjectsType, Some("odf"), Some("Objects"), scope).headOption.getOrElse(
+        val xmlReturn = scalaxb.toXML[xmlTypes.ObjectsType](odfObj.asObjectsType, Some("odf"), Some("Objects"), defaultScope).headOption.getOrElse(
           <error>Could not create from OdfObjects </error>)
         Some(Right(xmlReturn))
 
