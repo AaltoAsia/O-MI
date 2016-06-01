2af7f54b62a11c821327ba9dc13d89ea2d08d378
Tuomas Kinnunen <tuomas.kinnunen@aalto.fi>
2016-06-01 14:16:07 +0300

    fix callback retry test

--- O-MI Node/src/test/scala/responses/CallbackHandlerTest.scala
+++ O-MI Node/src/test/scala/responses/CallbackHandlerTest.scala
@@ -1,9 +1,11 @@
 package responses
 
-import scala.concurrent.Future
+import scala.concurrent.{Future, Await}
 import scala.concurrent.duration._
 
 import akka.actor.{ActorSystem, Props}
+import akka.util.Timeout
+import akka.pattern.ask
 import akka.io.IO
 import akka.testkit.TestProbe
 import org.specs2.concurrent.ExecutionEnv
@@ -17,7 +19,7 @@ class CallbackHandlerTest(implicit ee: ExecutionEnv) extends Specification {
       val port = 20003
       val probe = initCallbackServer(port)
       val msg  = <msg>success1</msg>
-      Thread.sleep(1000)
+
       CallbackHandlers.sendCallback(s"http://localhost:$port",msg,Duration(2, "seconds"))
 
       probe.expectMsg(2 seconds, Option(msg))
@@ -26,23 +28,23 @@ class CallbackHandlerTest(implicit ee: ExecutionEnv) extends Specification {
     "Try to keep sending message until ttl is over" in new Actors {
       val port = 20004
       val msg  = <msg>success2</msg>
-      Future{CallbackHandlers.sendCallback(s"http://localhost:$port", msg, Duration(10, "seconds"))}
+      CallbackHandlers.sendCallback(s"http://localhost:$port", msg, Duration(10, "seconds"))
 
-      Future{
+      val testProbeFuture = Future{
         Thread.sleep(4000)
         initCallbackServer(port)
       }
 
-
-      //val probe = initCallbackServer(port)
-      //probe.expectMsg(5 seconds, Option(msg))
+      val probe = Await.result(testProbeFuture, 5 seconds)
+      probe.expectMsg(5 seconds, Option(msg))
     }
   }
 
   def initCallbackServer(port: Int)(implicit system: ActorSystem): TestProbe = {
       val probe = TestProbe()
       val testServer = system.actorOf(Props(classOf[SystemTestCallbackServer], probe.ref))
-      IO(Http) ! Http.Bind(testServer, interface = "localhost", port = 20003)
+      implicit val timeout = Timeout(1 seconds)
+      IO(Http) ? Http.Bind(testServer, interface = "localhost", port = port)
       probe
   }
 }
