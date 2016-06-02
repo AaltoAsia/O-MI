7f6e2ab4f40d3c8e889f0397d107d8b608806916
Roman Filippov <filiroman.tsu@gmail.com>
2016-06-02 14:16:17 +0300

    user session retrieval

--- O-MI Node/html/webclient/js/usermenu.js
+++ O-MI Node/html/webclient/js/usermenu.js
@@ -1,13 +1,30 @@
 (function() {
     $(function() {
 
+      var currentUser;
+
       if (document.cookie == "")
       {
         $('<li><a href="/security/Login">Login</a></li>').appendTo('#user_menu');
       } else {
-        $('<li><a>'+document.cookie+'</a></li>').appendTo('#user_menu');
-        $('<li><a href="/security/Login">Logout</a></li>').appendTo('#user_menu');
-      }
 
+        $.ajax({
+          type: "GET",
+          url: "/security/PermissionService?getUserInfo=true",
+          contentType: "text/xml",
+          processData: false,
+          dataType: "text",
+          error: function(response) {
+            console.log(response);
+          },
+          success: function(response) {
+
+              console.log("Read current user successfully! Response:"+response);
+              currentUser = JSON.parse(response);
+              $('<li><a>'+currentUser['username']+'</a></li>').appendTo('#user_menu');
+              $('<li><a href="/security/Login">Logout</a></li>').appendTo('#user_menu');
+          }
+        });
+      }
     });
 }).call(this);
