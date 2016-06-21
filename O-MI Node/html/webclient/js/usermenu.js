b6f543bbe4aa0b15ff6059c3ce582fc7b98aa54c
Roman Filippov <filiroman.tsu@gmail.com>
2016-06-21 12:34:01 +0300

    js fixes

--- O-MI Node/html/webclient/js/usermenu.js
+++ O-MI Node/html/webclient/js/usermenu.js
@@ -8,6 +8,8 @@
         $('<li><a href="/security/Login">Login</a></li>').appendTo('#user_menu');
       } else {
 
+        console.log("Found cookie:" + document.cookie);
+
         $.ajax({
           type: "GET",
           url: "/security/PermissionService?getUserInfo=true",
@@ -20,9 +22,14 @@
           success: function(response) {
 
               console.log("Read current user successfully! Response:"+response);
-              currentUser = JSON.parse(response);
-              $('<li><a>'+currentUser['username']+'</a></li>').appendTo('#user_menu');
-              $('<li><a href="/security/Login">Logout</a></li>').appendTo('#user_menu');
+
+              if (response == "") {
+                $('<li><a href="/security/Login">Login</a></li>').appendTo('#user_menu');
+              } else {
+                currentUser = JSON.parse(response);
+                $('<li><a>'+currentUser['username']+'</a></li>').appendTo('#user_menu');
+                $('<li><a href="/security/Login?logout=true">Logout</a></li>').appendTo('#user_menu');
+              }
           }
         });
       }
