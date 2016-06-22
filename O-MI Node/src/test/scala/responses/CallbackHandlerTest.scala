5badf12abe51414e0f6035b797d0305f5f3481b1
Jussi Pirilä <jussi.pirila@aalto.fi>
2016-06-22 16:20:11 +0300

    fix few typos

--- O-MI Node/src/test/scala/responses/CallbackHandlerTest.scala
+++ O-MI Node/src/test/scala/responses/CallbackHandlerTest.scala
@@ -11,6 +11,9 @@ import org.specs2.mutable._
 import testHelpers.{Actors, SystemTestCallbackServer}
 
 class CallbackHandlerTest(implicit ee: ExecutionEnv) extends Specification {
+
+  sequential
+
   "CallbackHandler" should {
 
     "Send callback to the correct address" in new Actors {
@@ -29,7 +32,7 @@ class CallbackHandlerTest(implicit ee: ExecutionEnv) extends Specification {
       CallbackHandlers.sendCallback(s"http://localhost:$port", msg, Duration(10, "seconds"))
 
       val testProbeFuture = Future{
-        Thread.sleep(2000)
+        Thread.sleep(1000)
         initCallbackServer(port)
       }
 
