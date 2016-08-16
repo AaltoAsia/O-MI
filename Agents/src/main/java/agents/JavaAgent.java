97bee3212848f5a72c2bc3a33b2a421270df9d80
Tuomas Kinnunen <tuomas.kinnunen@aalto.fi>
2016-08-16 12:34:48 +0300

    improve example java agent documentation

--- Agents/src/main/java/agents/JavaAgent.java
+++ Agents/src/main/java/agents/JavaAgent.java
@@ -33,12 +33,18 @@ import types.OdfFactory;
 import types.OmiFactory;
 import types.OdfTypes.OdfInfoItem;
 
+/**
+ * Pushes random numbers to given O-DF path at given interval.
+ * Can be used in testing or as a base for other agents.
+ */
 public class JavaAgent extends JavaInternalAgent {
-  /*
+  /**
    *  THIS STATIC METHOD MUST EXISTS FOR JavaInternalAgent. 
    *  WITHOUT IT JavaInternalAgent CAN NOT BE INITIALIZED.
-   *  Implement it in way that Akka recommends to.
+   *  Implement it in way that
+   *  <a href="http://doc.akka.io/docs/akka/current/java/untyped-actors.html#Recommended_Practices">Akka recommends to</a>.
    *
+   *  @param _config Contains configuration for this agent, as given in application.conf.
    */
   static public Props props(final Config _config) {
     return Props.create(new Creator<JavaAgent>() {
@@ -50,54 +56,75 @@ public class JavaAgent extends JavaInternalAgent {
       }
     });
   }
-  public JavaAgent( Config conf){
-    config = conf;
-    interval = new FiniteDuration( config.getDuration("interval", TimeUnit.SECONDS), TimeUnit.SECONDS);	
-    path = new Path(config.getString("path"));
-  }
+
   protected Config config;
+
   protected FiniteDuration interval;
+
   protected Path path;
 
-  protected Cancellable job = null;
-  /*
+  protected Cancellable intervalJob = null;
+
+  //Random for generating new values for path.
+  protected Random rnd = new Random();
+
+
+  // Constructor
+  public JavaAgent(Config conf){
+    config = conf;
+
+    // Parse configuration for interval
+    interval = new FiniteDuration(
+            config.getDuration("interval", TimeUnit.SECONDS),
+            TimeUnit.SECONDS);	
+
+    // Parse configuration for target O-DF path
+    path = new Path(config.getString("path"));
+  }
+
+
+  /**
    * Method to be called when a Start() message is received.
    */
   @Override
   public InternalAgentResponse start() throws StartFailed {
     try{
       //Lets schelude a messge to us on every interval
-      //and save it for possibility that we want to stop agent.
-      job = context().system().scheduler().schedule(
-        Duration.Zero(),//Delay start
-        interval,//Interval between messages
-        self(),//To 
-        "Update",//Message, preferably immutable.
+      //and save the reference so we can stop the agent.
+      intervalJob = context().system().scheduler().schedule(
+        Duration.Zero(),                //Delay start
+        interval,                       //Interval between messages
+        self(),                         //To 
+        "Update",                       //Message, preferably immutable.
         context().system().dispatcher(),//ExecutionContext, Akka
-        null//Sender?
+        null                            //Sender?
       );
+
       return new CommandSuccessful();
-    } catch( Exception exp ) {
+
+    } catch (Exception exp) {
       //Normally in Akka if exception is thrown in child actor, it is 
-      //passed to its parent. That uses SupervisorStrategy to decide 
-      //what to do. With StartFailed we can tell AgentSystem that an 
-      //Exceptian was thrown during handling of Start() message.
-      return new StartFailed( exp.getMessage(), exp);
+      //passed to its parent. That uses {@link SupervisorStrategy} to decide 
+      //what to do. With {@link StartFailed} we can tell AgentSystem that an 
+      //Exception was thrown during handling of Start() message.
+      return new StartFailed(exp.getMessage(), exp);
     }
   }
 
-  /*
+
+  /**
    * Method to be called when a Stop() message is received.
+   * This should gracefully stop all activities that the agent is doing.
    */
   @Override
-  public InternalAgentResponse stop()  throws CommandFailed {
+  public InternalAgentResponse stop() throws CommandFailed {
 
-    if( job != null){//Job is defined? 
-      job.cancel(); //Cancel job
+    if (intervalJob != null){//is defined? 
+      intervalJob.cancel();  //Cancel intervalJob
       
-      //Check if job was cancelled
-      if( job.isCancelled() ){
-        job = null;
+      // Check if intervalJob was cancelled
+      if( intervalJob.isCancelled() ){
+        intervalJob = null;
       } else {
         return new CommandFailed("Failed to stop agent.");
       }
@@ -105,72 +132,100 @@ public class JavaAgent extends JavaInternalAgent {
     return new CommandSuccessful();
   }
 
-  //Random for generating new values for path.
-  Random rnd = new Random();
-  /*
+
+  /**
    * Method to be called when a "Update" message is received.
+   * Made specifically for this agent to be used with the Akka scheduler.
    */
   public void update() {
 
-    //Generate new OdfValue 
-    Timestamp timestamp =  new Timestamp(  new java.util.Date().getTime() );
+    // Generate new OdfValue 
+
+    // timestamp for the value
+    Timestamp timestamp =  new Timestamp(new java.util.Date().getTime());
+    // type metadata, default is xs:string
     String typeStr = "xs:integer";
+    // value as String
     String newValueStr = rnd.nextInt() +""; 
+
+    // Multiple values can be added at the same time but we add one
     Vector<OdfValue> values = new Vector();
-    OdfValue value = OdfFactory.createOdfValue( newValueStr, typeStr, timestamp);
+
+    OdfValue value = OdfFactory.createOdfValue(
+        newValueStr, typeStr, timestamp
+    );
     values.add(value);
 
-    //Create OdfInfoItem. 
+    // Create OdfInfoItem to contain the value. 
     OdfInfoItem infoItem = OdfFactory.createOdfInfoItem(
         path, 
         values
     );
 
-    //createAncestors generate O-DF structure from an OdfNode's path and retuns OdfObjects
+    // createAncestors generates O-DF structure from the path of an OdfNode 
+    // and returns the root, OdfObjects
     OdfObjects objects = infoItem.createAncestors();
 
-    log.debug( name + " pushing data...");
-    //Create O-MI write request
-    //interval as time to live
+    // This sends debug log message to O-MI Node logs if
+    // debug level is enabled (in logback.xml and application.conf)
+    log.debug(name + " pushing data...");
+
+    // Create O-MI write request
+    // interval as time to live
     WriteRequest write = OmiFactory.createWriteRequest(
-        interval, //ttl
-        objects //O-DF
+        interval, // ttl
+        objects   // O-DF
     );
     
+    // timeout for the write request, which means how long this agent waits for write results
     Timeout timeout = new Timeout(interval);
-    //We need to tell AgentSystem who is askinng request to be handled, so we wrap write
-    //in ResponsibilityRequest.
+
+    // We need to tell AgentSystem who is asking request to be handled, so we wrap write
+    // in ResponsibilityRequest.
     ResponsibilityRequest rw = new ResponsibilityRequest(name, write);
 
-    Future<ResponsibleAgentResponse> result = writeToNode(write,timeout);
+    // Execute the request, execution is asynchronous (will not block)
+    Future<ResponsibleAgentResponse> result = writeToNode(write, timeout);
 
     ExecutionContext ec = context().system().dispatcher();
-    //Call LogResult if write was successful.
+    // Call LogResult function (below) when write was successful.
     result.onSuccess(new LogResult(), ec);
 
   }
+
+
+  // Contains function for the asynchronous write result
   public final class LogResult extends OnSuccess<ResponsibleAgentResponse> {
       @Override public final void onSuccess(ResponsibleAgentResponse t) {
         log.debug(name + " pushed data successfully.");
       }
   }
+
   
-  /*
-   * Method that is inherited from akka.actor.UntyppedActor and handles incoming messages
+  /**
+   * Method that is inherited from akka.actor.UntypedActor and handles incoming messages
    * from other Actors.
    */
   @Override
   public void onReceive(Object message) throws StartFailed, CommandFailed {
     if( message instanceof Start) {
+      // Start is received when this agent should start it's functionality
       getSender().tell(start(),getSelf());
+
     } else if( message instanceof Stop) {
+      // Stop is received when this agent should stop it's functionality
       getSender().tell(stop(),getSelf());
+
     } else if( message instanceof Restart) {
+      // Restart is received when this agent should restart
+      // default behaviour is to call stop() and then start()
       getSender().tell(restart(),getSelf());
+
     } else if( message instanceof String) {
       String str = (String) message;
       if( str.equals("Update"))
         update();
+
     } else unhandled(message);
   }
 }
--- O-MI Node/src/main/java/agentSystem/JavaInternalAgent.java
+++ O-MI Node/src/main/java/agentSystem/JavaInternalAgent.java
@@ -14,6 +14,17 @@ import agentSystem.InternalAgent;
 import types.OmiTypes.WriteRequest;
 
 public abstract class JavaInternalAgent extends UntypedActor implements InternalAgent {
+  /**
+   *  static public Props props(final Config _config)
+   *  THIS STATIC METHOD MUST EXISTS FOR JavaInternalAgent. 
+   *  WITHOUT IT JavaInternalAgent CAN NOT BE INITIALIZED.
+   *  Implement it in way that
+   *  <a href="http://doc.akka.io/docs/akka/current/java/untyped-actors.html#Recommended_Practices">Akka recommends to</a>.
+   *
+   *  @param _config Contains configuration for this agent, as given in application.conf.
+   */
+  // TODO: static method cannot be defined in the interface?
+
   protected LoggingAdapter log = Logging.getLogger(getContext().system(), this);
   protected Config config;
   protected ActorRef agentSystem = context().parent();
