3d703d6be4a6f9a4026b81c382a3e6c5393ae25d
Jussi Pirilä <jussi.pirila@aalto.fi>
2016-11-22 18:15:40 +0200

    add prevayler functions for analytics

--- O-MI Node/src/main/scala/analytics/AnalyticsStore.scala
+++ O-MI Node/src/main/scala/analytics/AnalyticsStore.scala
@@ -14,16 +14,80 @@
 
 package analytics
 
-import org.prevayler.PrevaylerFactory
+import java.util.Date
+
+import org.prevayler.{Query, Transaction, PrevaylerFactory}
 import types.Path
 
 /**
  * Created by satsuma on 22.11.2016.
  */
 
-case class ReadFrequency(data: Map[Path, Long])
-case class WriteFrequency()
-class AnalyticsStore {
-  val writeStore = PrevaylerFactory.createTransientPrevayler(WriteFrequency())
-  val readStore = PrevaylerFactory.createTransientPrevayler(ReadFrequency())
+//Storage case classes
+
+/**
+ * Storage class for the access frequency data for the items in the O-DF tree
+ * @param data Map from value paths to the access timestamps in milliseconds since epoch,
+ *             length tells us the number of accesses and from timestamps we can calculate the average interval
+ */
+case class ReadFrequency(var data: Map[Path, Vector[Long]])
+object ReadFrequency {
+  def empty = ReadFrequency(Map.empty())
 }
+
+/**
+ * Storage class for the write frequency data for the items in the O-DF tree
+ * @param data Map from the value paths to the write timestamps in milliseconds since epoch,
+ *             length tells us the number of writes and from timestamps we can calculate the average interval
+ */
+case class WriteFrequency(var data: Map[Path,Vector[Long]])
+object WriteFrequency {
+  def empty = WriteFrequency(Map.empty())
+}
+
+
+
+
+
+
+
+//Prevaylers
+class AnalyticsStore {
+  val writeStore = PrevaylerFactory.createTransientPrevayler(WriteFrequency.empty)
+  val readStore = PrevaylerFactory.createTransientPrevayler(ReadFrequency.empty)
+}
+
+
+
+
+
+
+
+//Operations
+
+//update
+case class NewWrite(path: Path, timestamp: Long) extends Transaction[WriteFrequency] {
+  def executeOn(p: WriteFrequency, d: Date) = {
+    val newVal: Vector[Long] = p.data.get(path).fold(Vector(timestamp))(old => old :+ timestamp)
+    p.data = p.data.updated(path, newVal)
+  }
+}
+case class NewRead(path: Path, timestamp: Long) extends Transaction[ReadFrequency] {
+  def executeOn(p: ReadFrequency, d: Date) = {
+    val newVal: Vector[Long] = p.data.get(path).fold(Vector(timestamp))(old => old :+ timestamp)
+    p.data = p.data.updated(path, newVal)
+  }
+}
+
+//read
+case class GetReadFrequency(path: Path) extends Query[ReadFrequency, Vector[Long]] {
+  def query(p: ReadFrequency, d: Date): Vector[Long] = {
+    p.data.get(path).getOrElse(Vector.empty)
+  }
+}
+case class GetWriteFrequency(path: Path) extends Query[WriteFrequency, Vector[Long]] {
+  def query(p:WriteFrequency, d: Date): Vector[Long] = {
+    p.data.get(path).getOrElse(Vector.empty)
+  }
+}
+
