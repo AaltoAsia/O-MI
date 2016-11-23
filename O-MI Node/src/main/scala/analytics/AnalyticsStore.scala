20f85d410f205f3c3b145b47f24b5e2c15abe5d5
Jussi Pirilä <jussi.pirila@aalto.fi>
2016-11-23 14:53:03 +0200

    use STM for storing analytics data

--- O-MI Node/src/main/scala/analytics/AnalyticsStore.scala
+++ O-MI Node/src/main/scala/analytics/AnalyticsStore.scala
@@ -14,80 +14,45 @@
 
 package analytics
 
-import java.util.Date
 
-import org.prevayler.{Query, Transaction, PrevaylerFactory}
+import scala.concurrent.stm._
+
 import types.Path
 
-/**
- * Created by satsuma on 22.11.2016.
- */
 
-//Storage case classes
-
-/**
- * Storage class for the access frequency data for the items in the O-DF tree
- * @param data Map from value paths to the access timestamps in milliseconds since epoch,
- *             length tells us the number of accesses and from timestamps we can calculate the average interval
- */
-case class ReadFrequency(var data: Map[Path, Vector[Long]])
-object ReadFrequency {
-  def empty = ReadFrequency(Map.empty())
-}
-
-/**
- * Storage class for the write frequency data for the items in the O-DF tree
- * @param data Map from the value paths to the write timestamps in milliseconds since epoch,
- *             length tells us the number of writes and from timestamps we can calculate the average interval
- */
-case class WriteFrequency(var data: Map[Path,Vector[Long]])
-object WriteFrequency {
-  def empty = WriteFrequency(Map.empty())
-}
-
-
-
-
-
-
-
-//Prevaylers
 class AnalyticsStore {
-  val writeStore = PrevaylerFactory.createTransientPrevayler(WriteFrequency.empty)
-  val readStore = PrevaylerFactory.createTransientPrevayler(ReadFrequency.empty)
-}
 
+  //data window in seconds
+  private val dataWindow = ???
 
+  private val readSTM = TMap.empty[Path, Vector[Long]]
+  private val writeSTM = TMap.empty[Path,Vector[Long]]
 
-
-
-
-
-//Operations
-
-//update
-case class NewWrite(path: Path, timestamp: Long) extends Transaction[WriteFrequency] {
-  def executeOn(p: WriteFrequency, d: Date) = {
-    val newVal: Vector[Long] = p.data.get(path).fold(Vector(timestamp))(old => old :+ timestamp)
-    p.data = p.data.updated(path, newVal)
+  //private methods
+  private def getReadFrequency(path: Path): Vector[Long] = {
+    readSTM.single.get(path).toVector.flatten
   }
-}
-case class NewRead(path: Path, timestamp: Long) extends Transaction[ReadFrequency] {
-  def executeOn(p: ReadFrequency, d: Date) = {
-    val newVal: Vector[Long] = p.data.get(path).fold(Vector(timestamp))(old => old :+ timestamp)
-    p.data = p.data.updated(path, newVal)
-  }
-}
 
-//read
-case class GetReadFrequency(path: Path) extends Query[ReadFrequency, Vector[Long]] {
-  def query(p: ReadFrequency, d: Date): Vector[Long] = {
-    p.data.get(path).getOrElse(Vector.empty)
+  private def getWriteFrequency(path: Path): Vector[Long] = {
+    writeSTM.single.get(path).toVector.flatten
   }
-}
-case class GetWriteFrequency(path: Path) extends Query[WriteFrequency, Vector[Long]] {
-  def query(p:WriteFrequency, d: Date): Vector[Long] = {
-    p.data.get(path).getOrElse(Vector.empty)
-  }
-}
 
+ //public
+  def addRead(path: Path, timestamp: Long) = {
+    atomic{ implicit txn =>
+      val updated = readSTM.get(path).toVector.flatten :+ timestamp
+      readSTM.put(path, updated)
+    }
+  }
+  def addWrite(path: Path, timestamp: Long) = {
+    atomic{ implicit txn =>
+      val updated = readSTM.get(path).toVector.flatten :+ timestamp
+      readSTM.put(path, updated)
+    }
+  }
+
+
+
+
+
+}
