754e25500cc8163ffc8fa259e2bfd5ad110d9887
Jussi Pirilä <jussi.pirila@aalto.fi>
2017-05-24 16:35:27 +0300

    add more tests to callbacks

--- O-MI Node/src/test/scala/http/OmiServiceTest.scala
+++ O-MI Node/src/test/scala/http/OmiServiceTest.scala
@@ -45,7 +45,7 @@ class OmiServiceTest
   implicit val callbackHandler: CallbackHandler = new CallbackHandler(settings)( system, materializer)
   implicit val singleStores : SingleStores = new SingleStores(settings)
   implicit def default(implicit system: ActorSystem) = RouteTestTimeout(5.second)
-  implicit val dbConnection = new TestDB("omiServic-test")(
+  implicit val dbConnection = new TestDB("omiService-test")(
     system,
     singleStores,
     settings
