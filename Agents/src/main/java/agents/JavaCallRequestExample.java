9799223ab371aec71197b3d3f5a4439bf6f7d5d9
Tuomas Kinnunen <tuomas.kinnunen@aalto.fi>
2017-11-06 23:58:29 +0200

    New example java agent showing call and parsing json

--- Agents/src/main/java/agents/JavaCallRequestExample.java
+++ Agents/src/main/java/agents/JavaCallRequestExample.java
@@ -24,17 +24,210 @@ import types.OdfTypes.*;
 import types.OmiTypes.*;
 
 
+/**
+ * Requires following configuration in application.conf:
+ *  {
+ *    name = "JavaCallRequestExample" 
+ *    class = "agents.JavaCallRequestExample"
+ *    language = "java"
+ *     responsible = {
+ *       "Objects/Service/JavaCall" = "c"
+ *     }
+ *  }
+ *
+ *
+ *  Example request:
+ *  <omiEnvelope xmlns="http://www.opengroup.org/xsd/omi/1.0/" version="1.0" ttl="0">
+ *    <call msgformat="odf">
+ *      <msg>
+ *        <Objects xmlns="http://www.opengroup.org/xsd/odf/1.0/">
+ *          <Object>
+ *            <id>Service</id>
+ *            <InfoItem name="JavaCall">
+ *              <value type="odf">
+ *                <Objects>
+ *                  <Object>
+ *                    <id>MyParameters</id>
+ *                    <InfoItem name="MyParameter">
+ *                      <value type="xs:int">0</value>
+ *                    </InfoItem>
+ *                  </Object>
+ *                </Objects>
+ *             </value>
+ *           </InfoItem>
+ *         </Object>
+ *       </Objects>
+ *     </msg>
+ *   </call>
+ * </omiEnvelope>
+ */
 public class JavaCallRequestExample extends ResponsibleJavaInternalAgent {
+
+    // O-DF Path for the rpc
+    private static final Path callPath = new Path( "Objects/Service/JavaCall" );
+
+
+    // Some hardcoded json result data that are selected with a call parameter
+    private String[] myJsonSource = {
+                "{\"results\" : [{\"name\": \"first\", \"value\": 2}, {\"name\": \"second\", \"value\": 4}]}",
+                "{\"results\" : [{\"name\": \"first\", \"value\": 4}, {\"name\": \"second\", \"value\": 8}]}"};
+
+    // Classes representing the json data, used for gson library
+    class MyResultData {
+        public ArrayList<MyValue> results;
+    }
+
+    class MyValue {
+        public String name;
+        public int value;
+    }
+
+
+
+    // Json does not have one-to-one mapping to O-DF, so this coverts an example of domain specific json
+    public OdfObjects jsonToOdf(String json){
+
+        // google json library: https://github.com/google/gson/blob/master/UserGuide.md
+        Gson gson = new Gson();
+
+        // Parse json to MyResultData class
+        MyResultData myData = gson.fromJson(json, MyResultData.class);
+
+        // Use MyResultData to create the O-DF Objects
+        // The following O-DF tree is chosen to represent the json:
+        // * Objects
+        //   * Object "Results"
+        //     * InfoItem
+        //       * value
+        //     * InfoItem
+        //       * value
+        //     * ...
+        
+        ArrayList<OdfObject> objectsChildren = new ArrayList<OdfObject>();
+
+        // create Object "Results"
+        OdfObject myDataObject = myResultDataToOdf(myData);
+        objectsChildren.add(myDataObject);
+
+
+        OdfObjects objects = OdfFactory.createOdfObjects(objectsChildren, "1.0");
+        return objects;
+
+        // PRO TIP: Another way to create O-DF Objects hierarchies is to create only the leaf InfoItems
+        // then call .createAncestors() to each of the InfoItems to create a list of OdfObjects
+        // and then reduce the list to single OdfObjects with a.union(b).
+        // This might be more inefficent in terms of performance, but a lot easier/faster to code for simple O-DFs.
+        // Docs:
+        // https://otaniemi3d.cs.hut.fi/omi/node/html/api/java/types/OdfTypes/OdfNode.html#createAncestors--
+        // https://otaniemi3d.cs.hut.fi/omi/node/html/api/java/types/OdfTypes/OdfObjectsImpl.html#union-types.OdfTypes.OdfObjects-
+    }
+
+    private final ArrayList<OdfObject> emptyObjects = new ArrayList<OdfObject>();
+    //private final ArrayList<OdfInfoItem> emptyInfoItems = new ArrayList<OdfInfoItem>();
+
+    
+    public OdfObject myResultDataToOdf(MyResultData myData) {
+
+        Path resultsPath = new Path("Objects/Results");
+
+        // Results Object will have InfoItems as children
+        ArrayList<OdfInfoItem> infoItemChildren = new ArrayList<OdfInfoItem>();
+
+        for (MyValue myVal: myData.results) {
+
+            String name = myVal.name;
+            Path path = new Path("Objects/Results/"+name);
+
+            ArrayList<OdfValue<Object>> values = myValueToOdf(myVal);
+
+            OdfInfoItem child = OdfFactory.createOdfInfoItem(path, values);
+
+            infoItemChildren.add(child);
+        }
+
+        OdfObject myResultsObject = OdfFactory.createOdfObject(resultsPath, infoItemChildren, emptyObjects);
+
+        return myResultsObject;
+    }
+
+
+    public ArrayList<OdfValue<Object>> myValueToOdf(MyValue myVal) {
+
+        ArrayList<OdfValue<Object>> values = new ArrayList<OdfValue<Object>>();
+
+        // for ignoring the type, use String
+        OdfValue<Object> odfValue = OdfFactory.createOdfValue(myVal.value, getCurrentTime());
+        values.add(odfValue);
+
+        return values;
+    }
+
+
+
+    // This method is run when call request is received and application.conf settings
+    // are configured to route the request to this agent.
+    public Future<ResponseRequest> handleCall(CallRequest callRequest){
+
+
+        try{
+            // fetch the InfoItem containing the rpc parameters from the O-DF of the incoming call request
+            OdfNode node = callRequest.odf().get(callPath).get();
+
+            OdfInfoItem ii = (OdfInfoItem) node;
+
+            // The parameters are contained in another O-DF hierarchy inside the <value> element
+            OdfValue<Object> paramOdfValue = ii.values().head(); // head = first element
+
+            OdfObjects parameterOdf = (OdfObjects) paramOdfValue.value();
+
+            // Parameter should have the format:
+            // * Objects
+            //   * Object MyParameters
+            //     * InfoItem MyParameter
+
+            // Query O-DF with a Path
+            Path queryPath = new Path("Objects/MyParameters/MyParameter");
+
+            OdfInfoItem myParameterItem = (OdfInfoItem) parameterOdf.get(queryPath).get(); 
+            // Returns scala.Option, for which we call .get() as we don't care about errors atm.
+
+
+            // Try to parse (if the caller gave the parameter as xs:string type)
+            //int myParameter = Integer.parseInt((String) myParameterItem.values().head().value());
+            // ... Or if the <value> has right type attribute, it is already parsed:
+            int myParameter = (int) myParameterItem.values().head().value();
+
+            OdfObjects returnOdf = jsonToOdf(myJsonSource[myParameter]);
+
+            return Futures.successful( 
+                    Responses.Success(
+                        scala.Option.apply(returnOdf),
+                        Duration.apply(10,TimeUnit.SECONDS)
+                        )
+                    );
+        } catch(Exception exp ) {
+            return Futures.successful( 
+                    Responses.InternalError(exp)
+                    );
+        }
+    }
+
+
+
+    public Timestamp getCurrentTime() {
+        return new Timestamp(new java.util.Date().getTime());
+    }
+
 
     // Boilerplate for Akka Actors
     static public Props props(final Config config, final ActorRef requestHandler,final ActorRef dbHandler){
         return Props.create(
-                new Creator<ResponsibleJavaAgentDemo>() {
+                new Creator<JavaCallRequestExample>() {
                     private static final long serialVersionUID = 35735155L;
 
                     @Override
-                    public ResponsibleJavaAgentDemo create() throws Exception {
-                        return new ResponsibleJavaAgentDemo(config, requestHandler, dbHandler);
+                    public JavaCallRequestExample create() throws Exception {
+                        return new JavaCallRequestExample(config, requestHandler, dbHandler);
                     }
                 } 
         );
@@ -48,127 +241,6 @@ public class JavaCallRequestExample extends ResponsibleJavaInternalAgent {
 
         super(requestHandler,dbHandler);
 
-    }
-
-    public Timestamp getCurrentTime() {
-        return new Timestamp(new java.util.Date().getTime());
-    }
-
-    // Json does not have one-to-one mapping to O-DF, but this tries to transform some use cases
-    public OdfNode jsonToOdf(JsonObject jsonElement){
-
-
-        ArrayList<OdfObject> children = new ArrayList<OdfObject>();
-
-        for (Map.Entry<String,JsonElement> entry: jsonElement.entrySet()) {
-            String id = entry.getKey();
-
-            // First level children MUST be <Object>s in the O-DF standard
-            JsonObject jsonChild = (JsonObject) entry.getValue();
-
-            OdfObject child = (OdfObject) jsonToOdf(jsonChild, new Path("Objects/"+id));
-            children.add(child);
-        }
-
-        OdfObjects objects = OdfFactory.createOdfObjects(children, "1.0");
-        return objects;
-    }
-
-    // Recursive conversion
-    public OdfNode jsonToOdf(JsonElement jsonElement, Path parentPath){
-
-        if (jsonElement instanceof JsonObject) {
-            JsonObject jsonObj = (JsonObject) jsonElement;
-            // Object will be converted to O-DF Object
-            Path objectPath = new Path(parentPath.toString()+"/"+id); // TODO id
-
-            ArrayList<OdfObject> objectChildren = new ArrayList<OdfObject>();
-            ArrayList<OdfInfoItem> infoItemChildren = new ArrayList<OdfInfoItem>();
-
-            for (Map.Entry<String,JsonElement> entry: jsonObj.entrySet()) {
-
-                String id = entry.getKey();
-                JsonElement jsonChild = entry.getValue();
-
-                OdfNode child = jsonToOdf(jsonChild, objectPath);
-                if (child instanceof OdfObject)
-                    objectChildren.add((OdfObject)child);
-                else if (child instanceof OdfInfoItem)
-                    infoItemChildren.add((OdfInfoItem)child);
-            }
-
-            OdfObject obj = OdfFactory.createOdfObject(objectPath, infoItemChildren, objectChildren);
-            return obj;
-
-        } else if (jsonElement instanceof JsonArray){
-            JsonArray jsonArr = (JsonArray) jsonElement;
-            // Array will be converted to O-DF InfoItem with values or Object if it contains objects
-
-            ArrayList<OdfNode> values = new ArrayList<OdfNode>();
-
-            for (JsonElement arrElem: jsonArr) {
-                if (!(arrElem instanceof JsonPrimitive)) {
-                    throw new java.lang.UnsupportedOperationException("Don't know what to use as Object ID. Domain specific json converting needed.");
-                }
-                OdfNode value = jsonToOdf(arrElem, parentPath); 
-            }
-
-
-        } else if (jsonElement instanceof JsonPrimitive) {
-            JsonPrimitive jsonPrim = (JsonPrimitive) jsonElement;
-            // primitive will be converted to string value, TODO: add other types
-            System.out.println("JSON test:");
-            System.out.println(jsonPrim.toString());
-            System.out.println(jsonPrim.getAsString());
-            return OdfFactory.createOdfValue(jsonPrim.getAsString(), getCurrentTime());
-
-        } else if (jsonElement instanceof JsonNull) {
-            // null will be converted to empty value
-            return OdfFactory.createOdfValue("", getCurrentTime());
-        }
-    }
-
-    // O-DF Path for the rpc
-    private static final Path callPath = new Path( "Objects/Service/JavaCall" );
-
-    // Some hardcoded result data that are selected with a call parameter
-    private ArrayList myJsonSource = new ArrayList("{\"result\" : [\"hello\"}", "{}");
-
-    // This method is run when call request is received and application.conf settings
-    // are configured to route the request to this agent.
-    public Future<ResponseRequest> handleCall(CallRequest callRequest){
-
-        // google json library: https://github.com/google/gson/blob/master/UserGuide.md
-        Gson gson = new Gson();
-
-        try{
-            // fetch the InfoItem containing the rpc parameters from the O-DF of the incoming call request
-            OdfNode node = callRequest.odf().get(callPath).get();
-            OdfInfoItem ii = (OdfInfoItem)node;
-
-            // The parameters are contained in another O-DF hierarchy inside the <value> element
-            OdfValue<Object> value = ii.values().head();
-
-            OdfValue<Object> newValue = OdfFactory.createOdfValue("derp", getCurrentTime());  
-
-            Vector<OdfValue<Object>> values = new Vector<OdfValue<Object>>();
-            values.add( newValue );
-            OdfInfoItem new_ii = OdfFactory.createOdfInfoItem(
-                    new Path("Objects/Service/Greeting"),
-                    values 
-                    );
-
-            return Futures.successful( 
-                    Responses.Success(
-                        scala.Option.apply(new_ii.createAncestors()),
-                        Duration.apply(10,TimeUnit.SECONDS)
-                        )
-                    );
-        } catch(Exception exp ) {
-            return Futures.successful( 
-                    Responses.InternalError(exp)
-                    );
-        }
     }
 }
 
