ab63430a0830a409f76250ba75925306a7e8531d
Lauri Isojärvi <lauri.isojarvi@iki.fi>
2018-07-31 15:31:54 +0300

    Added influx query format test

--- O-MI-Node/src/test/scala/database/influxDB/InfluxDBProtocolsTest.scala
+++ O-MI-Node/src/test/scala/database/influxDB/InfluxDBProtocolsTest.scala
@@ -40,12 +40,121 @@ class InfluxDBProtocolsTest( implicit ee: ExecutionEnv )
          }
        } 
      }
-     /*
-     "For reading:" >> {
+     "For queries:" >> {
         val n = 5
         val begin = Some(currentTimestamp)
         val end = Some(new Timestamp(currentTimestamp.getTime().toLong + 1000000))
-     }*/
+
+        val dbName = "test"
+        "create correct query, clause or expression for" >>{
+          "CreateDB"  >> {
+            CreateDB(dbName).query must beEqualTo( s"CREATE DATABASE $dbName" ) 
+          }
+          "ShowDBs" >> {
+            ShowDBs.query must beEqualTo( "SHOW DATABASES" )
+          }
+          "DropMeasurement" >> {
+            DropMeasurement(dbName ).query must beEqualTo( s"""DROP MEASUREMENT "$dbName"""" )
+          }
+          "SelectValue without any clauses" >> {
+            SelectValue( dbName, None, None, None ).query must beEqualTo(s"""SELECT value FROM "$dbName"""")
+          }
+          "LowerTimeBoundExpression" >> {
+            LowerTimeBoundExpression(timestamp).expression must beEqualTo(s"time >= '$timestamp'")
+          }
+          "UpperTimeBoundExpression" >> {
+            UpperTimeBoundExpression(timestamp).expression must beEqualTo(s"time <= '$timestamp'")
+          }
+          "DescTimeOrderByClause" >> {
+            DescTimeOrderByClause().clause must beEqualTo( s"ORDER BY time DESC" )
+          }
+          "LimitClause" >> {
+            LimitClause(n).clause must beEqualTo( s"LIMIT $n")
+          }
+          "WhereClause with one expression" >> {
+            WhereClause( 
+              Vector( 
+                LowerTimeBoundExpression(timestamp)
+              )
+            ).clause must beEqualTo(s"WHERE time >= '$timestamp'")
+          }
+          "WhereClause with two expressions" >>{
+            WhereClause( 
+              Vector( 
+                LowerTimeBoundExpression(timestamp),
+                UpperTimeBoundExpression(timestamp)
+              )
+            ).clause must beEqualTo(s"WHERE time >= '$timestamp' AND time <= '$timestamp'")
+          }
+          "SelectValue with where clause" >>{
+            val where = WhereClause( 
+              Vector( 
+                LowerTimeBoundExpression(timestamp),
+                UpperTimeBoundExpression(timestamp)
+              )
+            )
+            SelectValue( dbName, Some(where), None, None).query must beEqualTo(
+              s"""SELECT value FROM "$dbName" ${where.clause}"""
+            )
+          }
+          "SelectValue with order by clause" >>{
+            val orderBy =DescTimeOrderByClause()
+            SelectValue( dbName, None, Some(orderBy), None ).query must beEqualTo(
+              s"""SELECT value FROM "$dbName" ${orderBy.clause}"""
+            )
+          }
+          "SelectValue with limit clause" >>{
+            val limit = LimitClause( n)
+            SelectValue( dbName, None, None, Some(limit)).query must beEqualTo(
+              s"""SELECT value FROM "$dbName" ${limit.clause}"""
+            )
+          }
+          "SelectValue with where and order by clause" >>{
+            val where = WhereClause( 
+              Vector( 
+                LowerTimeBoundExpression(timestamp),
+                UpperTimeBoundExpression(timestamp)
+              )
+            )
+            val orderBy =DescTimeOrderByClause()
+            SelectValue( dbName, Some(where), Some(orderBy), None).query must beEqualTo(
+              s"""SELECT value FROM "$dbName" ${where.clause} ${orderBy.clause}"""
+            )
+          }
+          "SelectValue with where and limit clause" >>{
+            val where = WhereClause( 
+              Vector( 
+                LowerTimeBoundExpression(timestamp),
+                UpperTimeBoundExpression(timestamp)
+              )
+            )
+            val limit = LimitClause( n)
+            SelectValue( dbName, Some(where),  None, Some(limit)).query must beEqualTo(
+              s"""SELECT value FROM "$dbName" ${where.clause} ${limit.clause}"""
+            )
+          }
+          "SelectValue with order by and limit clauses" >>{
+            val orderBy =DescTimeOrderByClause()
+            val limit = LimitClause( n)
+            SelectValue( dbName, None, Some(orderBy), Some(limit) ).query must beEqualTo(
+              s"""SELECT value FROM "$dbName" ${orderBy.clause} ${limit.clause}"""
+            )
+          }
+          "SelectValue with where, order by and limit clause" >>{
+            val where = WhereClause( 
+              Vector( 
+                LowerTimeBoundExpression(timestamp),
+                UpperTimeBoundExpression(timestamp)
+              )
+            )
+            val orderBy =DescTimeOrderByClause()
+            val limit = LimitClause( n)
+            SelectValue( dbName, Some(where), Some(orderBy), Some(limit)).query must beEqualTo(
+              s"""SELECT value FROM "$dbName" ${where.clause} ${orderBy.clause} ${limit.clause}"""
+            )
+          }
+        }
+     }
    }
 
 }
