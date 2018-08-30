680ef2ae22376ef3cb38670c234a3ae850bcc191
Lauri Isojärvi <lauri.isojarvi@iki.fi>
2018-08-30 15:37:02 +0300

    Remove parking facilities without spaces and capacities from results

--- Agents/src/main/scala/agents/parking/ParkingAgent.scala
+++ Agents/src/main/scala/agents/parking/ParkingAgent.scala
@@ -533,6 +533,11 @@ class ParkingAgent(
                       odf.nodesWithType(s"mv:RealTimeCapacity")
                     }.map(_.path).filterNot{
                       path => correctParkingSpaceAndCapacityPaths.contains(path)
+                    } ++ pfPaths.filterNot{
+                      path => correctParkingSpaceAndCapacityPaths.contains(path)
+                      correctParkingSpaceAndCapacityPaths.exists{
+                        foundPath => path.isAncestorOf(foundPath)
+                      }
                     }
 
                     val correctNodes = odf -- ( unwantedCapacitiesAndSpaces.toSeq)
