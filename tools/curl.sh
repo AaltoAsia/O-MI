14f42e419e0fedb9c7cb45e3d3e1a81d9018b260
Tuomas Kinnunen <tuomas.kinnunen@aalto.fi>
2019-07-12 15:27:17 +0300

    added optional arguments to curl script

--- tools/curl.sh
+++ tools/curl.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 # Send O-MI message from file to url
 
+filepath=$1
+shift
+nodeurl=$1
+shift
+
 usage() {
     echo "usage: $0 file url"
 }
 
-if [[ ! -f $1 ]]; then
+if [[ ! -f "$filepath" ]]; then
     echo "Invalid file given: $1"
     usage
     exit 1
 fi
-if [[ -z $2 ]]; then
+if [[ -z "$nodeurl" ]]; then
     echo "No url was given as second argument"
     usage
     exit 1
 fi
 
-curl --max-time 180 -X POST --header "Content-Type:text/xml;charset=UTF-8" -H "Transfer-Encoding:chunked" --data @"$1" "$2"
+curl --max-time 180 -X POST --header "Content-Type:text/xml;charset=UTF-8" -H "Transfer-Encoding:chunked" --data @"filepath" "$nodeurl" $*
 
