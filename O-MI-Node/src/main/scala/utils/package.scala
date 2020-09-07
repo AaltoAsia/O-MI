f8d8f105db519b0c2074c31c29220bff032f3a70
Jussi Pirilä <jussi.pirila@aalto.fi>
2020-09-07 12:21:09 +0300

    Add utility class for parsing optional config values

--- O-MI-Node/src/main/scala/utils/package.scala
+++ O-MI-Node/src/main/scala/utils/package.scala
@@ -1,14 +1,17 @@
 import scala.language.implicitConversions
 import scala.util.Try
 import java.sql.Timestamp
-import java.time.{ZoneId, OffsetDateTime, LocalDateTime}
+import java.time.{LocalDateTime, OffsetDateTime, ZoneId}
+
 import javax.xml.datatype.DatatypeFactory
 import java.util.Date
+
 import akka.stream.scaladsl._
 import akka.util.ByteString
 import akka.NotUsed
 import akka.stream.alpakka.xml._
 import akka.stream.alpakka.xml.scaladsl.XmlWriting
+import com.typesafe.config.Config
 
 package object utils {
   def dateTimeStrToTimestamp(dateTimeString: String): Timestamp = {
@@ -37,4 +40,11 @@ package object utils {
       .filter(_.nonEmpty)
   }
   def parseEventsToStringSource( events: Iterable[ParseEvent] ): Source[String,NotUsed]  = parseEventsToByteSource(events).map[String](_.utf8String)
+
+  implicit class RichConfig(val underlying: Config) extends AnyVal {
+    def getOptionalString(path: String): Option[String] = if (underlying.hasPath(path)) Some(underlying.getString(path)) else None
+    def getOptionalInt(path: String): Option[Int] = if (underlying.hasPath(path)) Some(underlying.getInt(path)) else None
+    def getOptionalConfig(path: String): Option[Config] = if (underlying.hasPath(path)) Some(underlying.getConfig(path)) else None
+    def getOptionalBoolean(path: String): Option[Boolean] = if (underlying.hasPath(path)) Some(underlying.getBoolean(path)) else None
+  }
 }
